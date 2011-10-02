181fcefa32ca1bbb9685fab19e373de89fccf778
Floris Jansen <florismartijnjansen@gmail.com>
2011-10-02 14:29:57 +0200

    layouttoegevoegd

---
+++ layout/index.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<html>
+  <head>
+
+    <title>Cygnus Template</title>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+
+  </head>
+  <body>
+
+    <header>
+    	<a id="login" href="">Login</a>
+    </header>
+
+    <div id="wrapper">
+
+      <h1>Cygnus</h1>
+      <p>rdstfhj</p>
+
+    </div>
+    
+    <footer>
+    	<div id="center">
+	    	<ul class="sitemap">
+	    		<li>
+	    			<a href="">Home</a>
+	    		</li>
+	    		<li>
+	    			<a href="">Het cygnus</a>
+	    		</li>
+	    		<li>
+	    			<ul>
+	    				<li>
+			    			<a href="">Naam</a>
+	    				</li>
+	    				<li>
+			    			<a href="">Schoolgids</a>
+	    				</li>
+	    				<li>
+			    			<a href="">Vensters Verantwoording</a>
+	    				</li>
+	    				<li>
+			    			<a href="">Gebouw</a>
+	    				</li>
+	    				<li>
+			    			<a href="">...</a>
+	    				</li>
+	    			</ul>
+	    		</li>
+	    		<li>
+	    			<a href="">Onderwijs</a>
+	    		</li>
+	    		<li>
+	    			<ul>
+	    				<li>
+			    			<a href="">Start</a>
+	    				</li>
+	    				<li>
+			    			<a href="">Vernieuwing</a>
+	    				</li>
+	    				<li>
+			    			<a href="">...</a>
+	    				</li>
+	    			</ul>
+	    		</li>
+	    	</ul>
+	    </div>
+    </footer>
+
+  </body>
+</html>
