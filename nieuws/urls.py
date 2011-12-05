7497257f6908c72403c90bc357caa5646058de11
Martijn Arts <arts.martijn@gmail.com>
2011-12-05 10:00:26 +0100

    Added detail view for nieuwsbericht

--- nieuws/urls.py
+++ nieuws/urls.py
@@ -4,18 +4,24 @@ from django.views.generic.date_based import archive_index, archive_year, archive
 from cygy.nieuws.models import NieuwsBericht
 
 urlpatterns = patterns('',
-    (r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[-\w]*)/$', object_detail,
-        {'queryset': NieuwsBericht.objects.all(), 'date_field': 'publiceer', 'month_format': '%m'}, 'nieuwsbericht-detail'),
+    (r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[-\w]*)/$',
+        object_detail,
+        {'queryset': NieuwsBericht.objects.all(), 'date_field': 'publiceer',
+         'month_format': '%m', 'template_object_name': 'bericht'},
+         'nieuwsbericht-detail'),
 
     (r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/$', archive_day,
-        {'queryset': NieuwsBericht.objects.all(), 'date_field': 'publiceer', 'month_format': '%m'}),
+        {'queryset': NieuwsBericht.objects.all(), 'date_field': 'publiceer',
+         'month_format': '%m'}),
 
     (r'^(?P<year>\d{4})/(?P<month>\d{2})/$', archive_month,
-        {'queryset': NieuwsBericht.objects.all(), 'date_field': 'publiceer', 'month_format': '%m'}),
+        {'queryset': NieuwsBericht.objects.all(), 'date_field': 'publiceer',
+         'month_format': '%m'}),
 
     (r'^(?P<year>\d{4})/$', archive_year,
         {'queryset': NieuwsBericht.objects.all(), 'date_field': 'publiceer'}),
 
     (r'^$', archive_index,
-        {'queryset': NieuwsBericht.objects.all(), 'date_field': 'publiceer', 'num_latest': 10}),
+        {'queryset': NieuwsBericht.objects.all(), 'date_field': 'publiceer',
+         'num_latest': 10}),
 )
