8cd2410a43b1c3d8b3614e2186569cbfdab62396
Martijn Arts <arts.martijn@gmail.com>
2011-12-06 09:58:02 +0100

    Renamed models

--- news/models.py
+++ news/models.py
@@ -2,22 +2,18 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class NieuwsBericht(models.Model):
-    schrijver = models.ForeignKey(User)
-    titel = models.CharField(max_length=255)
-    slug = models.SlugField(unique_for_date='publiceer', help_text='URL-vriendelijke versie van de titel, kan meestal met rust gelaten worden.')
-    publiceer = models.DateTimeField(auto_now_add=True)
+class NewsMessage(models.Model):
+    writer = models.ForeignKey(User)
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(unique_for_date='publish', help_text='URL-friendly version of the title, can be left alone most of the time.')
+    publish = models.DateTimeField(auto_now_add=True)
 
-    inhoud = models.TextField(help_text='Het nieuwsbericht.')
-    korte_inhoud = models.TextField(help_text='Korte samenvatting van het nieuwsbericht.')
-
-#    def get_absolute_url(self):
-#        return '/nieuws/{}/{}/{}/{}/'.format(
-#                    self.publiceer.year, self.publiceer.strftime('%m'), self.publiceer.strftime('%d'), self.slug)
+    content = models.TextField(help_text='Het nieuwsbericht.')
+    summary = models.TextField(help_text='Korte samenvatting van het nieuwsbericht.')
 
     @models.permalink
     def get_absolute_url(self):
-        return ('nieuwsbericht-detail', (), {
+        return ('newsmessage-detail', (), {
                     'year': self.publiceer.year,
                     'month': self.publiceer.strftime('%m'),
                     'day': self.publiceer.strftime('%d'),
@@ -25,6 +21,5 @@ class NieuwsBericht(models.Model):
 
 
     class Meta:
-        verbose_name = 'nieuwsbericht'
-        verbose_name_plural = 'nieuwsberichten'
+        verbose_name = 'news message'
 
