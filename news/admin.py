608295d0384171c1e832d4e7a346bc89ff04b481
Martijn Arts <arts.martijn@gmail.com>
2011-12-06 10:03:47 +0100

    fixed references in admin.py

--- news/admin.py
+++ news/admin.py
@@ -1,17 +1,17 @@
 from django.contrib import admin
-from cygy.nieuws.models import NieuwsBericht
+from cygy.news.models import NewsMessage
 
-class NieuwsBerichtAdmin(admin.ModelAdmin):
-    date_hierarchy = 'publiceer'
-    include = ('titel', 'inhoud',)
-    exclude = ('schrijver',)
-    prepopulated_fields = {"slug": ("titel",)}
+class NewsMessageAdmin(admin.ModelAdmin):
+    date_hierarchy = 'publish'
+    include = ('title', 'content',)
+    exclude = ('writer',)
+    prepopulated_fields = {"slug": ("title",)}
 
     def save_form(self, request, form, change):
-        obj = super(NieuwsBerichtAdmin, self).save_form(request, form, change)
+        obj = super(NewsMessageAdmin, self).save_form(request, form, change)
         if not change:
             obj.schrijver = request.user
         return obj
 
 
-admin.site.register(NieuwsBericht, NieuwsBerichtAdmin)
+admin.site.register(NewsMessage, NewsMessageAdmin)
