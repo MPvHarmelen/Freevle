8df6cd1c4bb604148826cf7c7af646eee11bd1f3
Martin van Harmelen <martin@vanharmelen.com>
2011-12-06 11:41:48 +0100

    Wrote class PageAdmin

--- cms/admin.py
+++ cms/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from cygy.cms.models import Page
 
 class PageAdmin(admin.ModelAdmin):
-    pass
+    prepopulated_fields = {"slug": ("title",)}
 
 
 admin.site.register(Page,PageAdmin)
