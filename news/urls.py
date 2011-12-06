f8f80023246c24fe19aa15152c4ce207454f5e5d
Martijn Arts <arts.martijn@gmail.com>
2011-12-06 10:01:58 +0100

    Fixed references in urls.py

--- news/urls.py
+++ news/urls.py
@@ -1,21 +1,21 @@
 from django.conf.urls.defaults import *
 from django.views.generic.date_based import archive_index, archive_year, archive_month, archive_day, object_detail
 
-from cygy.nieuws.models import NieuwsBericht
+from cygy.nieuws.models import NewsMessage
 
 urlpatterns = patterns('',
     (r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[-\w]*)/$', object_detail,
-        {'queryset': NieuwsBericht.objects.all(), 'date_field': 'publiceer', 'month_format': '%m'}, 'nieuwsbericht-detail'),
+        {'queryset': NewsMessage.objects.all(), 'date_field': 'publiceer', 'month_format': '%m'}, 'nieuwsbericht-detail'),
 
     (r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/$', archive_day,
-        {'queryset': NieuwsBericht.objects.all(), 'date_field': 'publiceer', 'month_format': '%m'}),
+        {'queryset': NewsMessage.objects.all(), 'date_field': 'publiceer', 'month_format': '%m'}),
 
     (r'^(?P<year>\d{4})/(?P<month>\d{2})/$', archive_month,
-        {'queryset': NieuwsBericht.objects.all(), 'date_field': 'publiceer', 'month_format': '%m'}),
+        {'queryset': NewsMessage.objects.all(), 'date_field': 'publiceer', 'month_format': '%m'}),
 
     (r'^(?P<year>\d{4})/$', archive_year,
-        {'queryset': NieuwsBericht.objects.all(), 'date_field': 'publiceer'}),
+        {'queryset': NewsMessage.objects.all(), 'date_field': 'publiceer'}),
 
     (r'^$', archive_index,
-        {'queryset': NieuwsBericht.objects.all(), 'date_field': 'publiceer', 'num_latest': 10}),
+        {'queryset': NewsMessage.objects.all(), 'date_field': 'publiceer', 'num_latest': 10}),
 )
