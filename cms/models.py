b8245a4dc2f566e1bca6965baaae3b531ccf9703
Martin van Harmelen <martin@vanharmelen.com>
2011-12-06 10:02:18 +0100

    Added page moddel to cms app

--- cms/models.py
+++ cms/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Page(models.Model):
+    last_edit = models.DatTimeField(auto_now=True)
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True, help_text='URL-friendly version of the title, can be left alone most of the time.')
+    content = models.TextField(help_text='This is the content.')
+    
