4b99959c94c0133185c6c64dc965874fbe092d48
Martin van Harmelen <martin@vanharmelen.com>
2011-12-08 13:47:02 +0100

    Added list_display to cms.admin.PageAdmin

--- cms/admin.py
+++ cms/admin.py
@@ -3,6 +3,7 @@ from cygy.cms.models import Page
 
 class PageAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
+    list_display = ('slug')
 
 
 admin.site.register(Page,PageAdmin)
