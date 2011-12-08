420326157ae84587d8cea7f05e0f4c8cbdf1688c
Martin van Harmelen <martin@vanharmelen.com>
2011-12-08 13:58:37 +0100

    Started writing cms.urls.py

--- cms/urls.py
+++ cms/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls.defaults import *
 from django.views.generic.list_detail import object_detail
 
 urlpatterns = patterns('',
-#    (r'^P?<slug>\d+/$'
+#    (r'^(P?<slug>\d+)/$', object_detail, #And what the hell is the queryset?
      )
