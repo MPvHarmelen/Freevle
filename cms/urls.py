8400fa86dfcf584d179dbf9106ac717fa242cd0c
Martin van Harmelen <Martin@vanharmelen.com>
2011-12-08 22:08:26 +0100

    Wrote cms.urls.py, but...
    Django doesn't seem to think the way I do =( Can you tell what is wrong
    with my syntax?

--- cms/urls.py
+++ cms/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls.defaults import *
 from django.views.generic.list_detail import object_detail
+from cygy.cms.models import Page
 
 urlpatterns = patterns('',
-#    (r'^(P?<slug>\d+)/$', object_detail, #And what the hell is the queryset?
-     )
+    (r'(P?<slug>\d+)', object_detail, {'queryset': Page.objects.all(),
+                                          'slug': '%(slug)s'
+     }))
