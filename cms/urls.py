d1ee6532034a9876827570a83668a8da08bfd079
Martin van Harmelen <martin@vanharmelen.com>
2011-12-09 09:43:46 +0100

    Sorted out models.urls, but showing content doesn't yet work

--- cms/urls.py
+++ cms/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls.defaults import *
 from django.views.generic.list_detail import object_detail
+from django.views.generic.list_detail import object_list
 from cygy.cms.models import Page
 
 urlpatterns = patterns('',
-    (r'(P?<slug>\d+)', object_detail, {'queryset': Page.objects.all(),
-                                          'slug': '%(slug)s'
-     }))
+    (r'^$', object_list, {'queryset': Page.objects.all()}),
+    (r'^(?P<slug>[\w-]+)/$', object_detail, {'queryset': Page.objects.all()}),
+)
