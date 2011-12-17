387df6d6276b98d79e4de85bd1610a7e4f6978b6
Martin van Harmelen <Martin@vanharmelen.com>
2011-12-17 18:47:18 +0100

    Tried to fix news app

--- cms/urls.py
+++ cms/urls.py
@@ -5,6 +5,6 @@ from django.views.generic.list_detail import object_list
 from cygy.cms.models import Page
 
 urlpatterns = patterns('',
-    (r'^$', object_list, {'queryset': Page.objects.all()}),
+    (r'^$', object_list, {'queryset': Page.objects.all(), 'template_object_name':'Page'}),
     (r'^(?P<slug>[\w-]+)/$', object_detail, {'queryset': Page.objects.all(), 'template_object_name':'Page'}),
 )
--- news/urls.py
+++ news/urls.py
@@ -6,22 +6,22 @@ from cygy.news.models import NewsMessage
 urlpatterns = patterns('',
     (r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[-\w]*)/$',
         object_detail,
-        {'queryset': NewsMessage.objects.all(), 'date_field': 'publiceer',
-         'month_format': '%m', 'template_object_name': 'bericht'},
+        {'queryset': NewsMessage.objects.all(), 'date_field': 'publish',
+         'month_format': '%m', 'template_object_name': 'message'},
          'newsmessage-detail'),
 
     (r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/$', archive_day,
-        {'queryset': NewsMessage.objects.all(), 'date_field': 'publiceer',
+        {'queryset': NewsMessage.objects.all(), 'date_field': 'publish',
          'month_format': '%m'}),
 
     (r'^(?P<year>\d{4})/(?P<month>\d{2})/$', archive_month,
-        {'queryset': NewsMessage.objects.all(), 'date_field': 'publiceer',
+        {'queryset': NewsMessage.objects.all(), 'date_field': 'publish',
          'month_format': '%m'}),
 
     (r'^(?P<year>\d{4})/$', archive_year,
-        {'queryset': NewsMessage.objects.all(), 'date_field': 'publiceer'}),
+        {'queryset': NewsMessage.objects.all(), 'date_field': 'publish'}),
 
     (r'^$', archive_index,
-        {'queryset': NewsMessage.objects.all(), 'date_field': 'publiceer',
+        {'queryset': NewsMessage.objects.all(), 'date_field': 'publish',
          'num_latest': 10}),
 )
