03bf6736d0ba7d656c084f76175a1d1b7d1cd21c
Martin van Harmelen <Martin@vanharmelen.com>
2011-12-17 18:29:57 +0100

    Added template_object_name parameter to cms.urls.py

--- cms/urls.py
+++ cms/urls.py
@@ -6,5 +6,5 @@ from cygy.cms.models import Page
 
 urlpatterns = patterns('',
     (r'^$', object_list, {'queryset': Page.objects.all()}),
-    (r'^(?P<slug>[\w-]+)/$', object_detail, {'queryset': Page.objects.all()}),
+    (r'^(?P<slug>[\w-]+)/$', object_detail, {'queryset': Page.objects.all(), 'template_object_name':'Page'}),
 )
