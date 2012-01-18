cfacf1cd9e063e776f1adb4532e72ed18addb775
Martin van Harmelen <martin@vanharmelen.com>
2012-01-18 16:11:41 +0100

    Started writing letters.models

--- letters/models.py
+++ letters/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Letter(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(unique_for_date='publish')
+    publish = models.DatTimeField(auto_now_add=True)
+    content = FileField(Upload_to='files/')
