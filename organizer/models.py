7db0f978c637a78aa804f0b362c137ca391bf584
Martin van Harmelen <martin@vanharmelen.com>
2012-01-30 13:30:43 +0100

    Organizer.models is finito!

--- organizer/models.py
+++ organizer/models.py
@@ -5,29 +5,39 @@ from django.utils.translation import ugettext_lazy as _
 
 
 # Create your models here.
-class Lesson(models.Model):
-	teacher = models.ForeignKey(User,
-			limit_choices_to={'groups_in': ['teachers']})
-	topic = models.ForeignKey(Topic)
-	pupils = models.ManyToMany(User,
-			limit_choices_to={'groups_in':['pupils']})
-	time = models.ManyToMany(Time)
+class Topic(models.Model):
+    name = models.CharField(max_length=32)
+    abbr = models.CharField(max_length=8)
 
-#class Time(models.Model):
-#	DAY_CHOICES = (
-#		('MON', _('Monday'))
-#		('TUE', _('Tuesday'))
-#		('WEN', _('Wednesday'))
-#		('THU', _('Thursday'))
-#		('FRI', _('Friday'))
-#	)
-#	day = models.CharField(max_length=3, choices=DAY_CHOICES)
-#	period = models.IntegerField()
+class Lesson(models.Model):
+    DAY_CHOICES = (
+        ('MON', _('Monday')),
+        ('TUE', _('Tuesday')),
+        ('WEN', _('Wednesday')),
+        ('THU', _('Thursday')),
+        ('FRI', _('Friday')),
+        ('SAT', _('Saturday')),
+        ('SUN', _('Sunday')),
+    )
+    teacher = models.ForeignKey(User,
+            limit_choices_to={'groups_in': ['teachers']})
+    topic = models.ForeignKey(Topic)
+    classroom = models.CharField(max_length=16)
+    period = models.IntegerField()
+    day_of_week = models.CharField(max_length=3, choices=DAY_CHOICES)
+
+class TimeTable(models.Model):
+    date_of_monday = models.DateField()
+    lessons = models.ManyToManyField(Lesson)
+    user = models.ForeignKey(User) 
+
+class HomeworkType(models.Model):
+    name = models.CharField(max_length=32)
+    abbr = models.CharField(max_length=8)
+    color = fields.HexColorField()
 
 class Homework(models.Model):
-	type = models.ForeignKey(HomeworkType)
-	content = models.CharField(max_length=255)
-	lesson = models.ForeignKey(Lesson)
-	time = models.ForeignKey
-	 
-	
+    type = models.ForeignKey(HomeworkType)
+    content = models.CharField(max_length=255)
+    timetable = models.ForeignKey(TimeTable) 
+    lesson = models.ForeignKey(Lesson)
