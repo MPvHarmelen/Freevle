6876edc90a424cb76b8ed57ac3a9793f63fafb5b
Martin van Harmelen <Martin@vanharmelen.com>
2012-01-31 21:51:55 +0100

    Wrote update_filename, but...
    It doesn't work because join gets to many arguments blabla

--- letters/models.py
+++ letters/models.py
@@ -1,8 +1,14 @@
 from django.db import models
+import os.path
 
 # Create your models here.
 class Letter(models.Model):
+    def update_filename(instance, filename):
+        path = "letters/"
+        format = instance.slug
+        return path.join(path, format)
+    
     title = models.CharField(max_length=255)
     slug = models.SlugField(unique_for_date='publish')
     publish = models.DateTimeField(auto_now_add=True)
-    content = models.FileField(upload_to='files/letters/')
+    content = models.FileField(upload_to=update_filename)
