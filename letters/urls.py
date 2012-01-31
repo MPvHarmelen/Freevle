70426eef37c6bc976a34d49f0d8ca5df54bff955
Martin van Harmelen <martin@vanharmelen.com>
2012-01-31 09:48:44 +0100

    Wrote letters.urls

---
+++ letters/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls.defaults import *
+from django.views.generic.date_based import archive_index
+
+from cygy.letters.models import Letter
+
+urlpatterns = patterns('',
+    (r'^$', archive_index,
+        {'queryset': Letter.objects.all(), 'date_field': 'publish',
+         'num_latest': 30}),
+)
