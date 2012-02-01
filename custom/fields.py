552abc8e962d4605532b696c881f852b82ef12fa
Martin van Harmelen <Martin@vanharmelen.com>
2012-02-01 16:42:29 +0100

    Changed tabs into 4 spaces of custom.fields

--- custom/fields.py
+++ custom/fields.py
@@ -4,26 +4,26 @@ from django.forms import ValidationError
 from django.utils.encoding import smart_unicode
 
 class HexColorField(fields.Field):
-	
-	default_error_messages = {
-		'hex_error': u'This is an invalid color code. It must be a html hex color code e.g. #000000'
-	}
+    
+    default_error_messages = {
+        'hex_error': u'This is an invalid color code. It must be a html hex color code e.g. #000000'
+    }
 
-	def clean(self, value):
-		
-		super(HexColorField, self).clean(value)
-		
-		if value in fields.EMPTY_VALUES:
-			return u''
-		
-		value = smart_unicode(value)
-		value_length = len(value)
-		
-		if value_length != 7 or not re.match('^\#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$', value):
-			raise ValidationError(self.error_messages['hex_error'])
-		
-		return value
+    def clean(self, value):
+        
+        super(HexColorField, self).clean(value)
+        
+        if value in fields.EMPTY_VALUES:
+            return u''
+        
+        value = smart_unicode(value)
+        value_length = len(value)
+        
+        if value_length != 7 or not re.match('^\#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$', value):
+            raise ValidationError(self.error_messages['hex_error'])
+        
+        return value
 
-	def widget_attrs(self, widget):
-		if isinstance(widget, (fields.TextInput)):
-			return {'maxlength': str(7)}
+    def widget_attrs(self, widget):
+        if isinstance(widget, (fields.TextInput)):
+            return {'maxlength': str(7)}
