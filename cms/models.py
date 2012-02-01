9cfd95650caf42130c257298564a002469113b01
Martin van Harmelen <Martin@vanharmelen.com>
2012-02-01 21:43:22 +0100

    Wrote Page.__undicode__ and limmeted parent

--- cms/models.py
+++ cms/models.py
@@ -3,8 +3,11 @@ from django.utils.translation import ugettext_lazy as _
 
 # Create your models here.
 class Page(models.Model):
-    parent = models.ForeignKey('self', null=True, blank=True)
+    parent = models.ForeignKey('self', null=True, blank=True, limit_choices_to={'parent':None})
     last_edit = models.DateTimeField(auto_now=True)
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=32)
     slug = models.SlugField(unique=True, help_text=_('URL-friendly version of the title, can be left alone most of the time.'))
     content = models.TextField(help_text=_('Content of the page.'))
+    
+    def __unicode__(self):
+        return self.title
