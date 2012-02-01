cdb24e50c65b5db8a3ac2edb040123472384bccd
Martin van Harmelen <Martin@vanharmelen.com>
2012-02-01 18:03:31 +0100

    Added a pdf validator to Letter

--- letters/models.py
+++ letters/models.py
@@ -1,14 +1,22 @@
 from django.db import models
-import os.path
+from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext_lazy as _
 
 # Create your models here.
 class Letter(models.Model):
+    
+    def validate_pdf(value):
+        extension = value.url.split('.')[-1].lower()
+        if extension != 'pdf':
+            raise ValidationError(_('This should be a pdf file'))
+    
     def update_filename(instance, filename):
-        path = "letters/"
-        format = instance.slug
-        return path.join(path, format)
+        path = 'letters/'
+        new_file_name = instance.slug
+        extension = filename.split('.')[-1]
+        return path + new_file_name + '.' + extension
     
     title = models.CharField(max_length=255)
     slug = models.SlugField(unique_for_date='publish')
     publish = models.DateTimeField(auto_now_add=True)
-    content = models.FileField(upload_to=update_filename)
+    content = models.FileField(upload_to=update_filename, validators=[validate_pdf])
