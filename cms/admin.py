66bb641d4f8255fa7841b64eb7965a75fcf16d24
Martin van Harmelen <martin@vanharmelen.com>
2012-02-03 14:11:25 +0100

    Added parent to PageAdmin.list_display

--- cms/admin.py
+++ cms/admin.py
@@ -3,7 +3,7 @@ from cygy.cms.models import Page
 
 class PageAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
-    list_display = ('title', )
+    list_display = ('title', 'parent',)
 
 
 admin.site.register(Page,PageAdmin)
