70cf7fc5bad0ded59c6e86f0fad77c70ff586737
Martin van Harmelen <martin@vanharmelen.com>
2012-02-03 15:11:20 +0100

    Changed cms.urls to use PageView

--- cms/urls.py
+++ cms/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls.defaults import *
-from django.views.generic.list_detail import object_detail
+from cygy.cms.views import PageView
 from django.views.generic.list_detail import object_list
 
 from cygy.cms.models import Page
 
 urlpatterns = patterns('',
     (r'^$', object_list, {'queryset': Page.objects.all(), 'template_object_name':'Page'}),
-    (r'^(?P<slug>[\w-]+)/$', object_detail, {'queryset': Page.objects.all(), 'template_object_name':'Page'}),
+    (r'^(?P<parent>[\w-]+)/(?P<slug>[\w-]+)/$', PageView.as_view(model=Page)),
 )
