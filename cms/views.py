8d87554dd54c83f67256db68ba728b8871d6feb0
Martin van Harmelen <martin@vanharmelen.com>
2012-02-03 15:11:47 +0100

    Started writing cms.views.PageView

--- cms/views.py
+++ cms/views.py
@@ -1,1 +1,6 @@
 # Create your views here.
+from django.views.generic.detail import DetailView
+
+def PageView(DetailView):
+    def get_queryset(self):
+        return self.model._default_manager.all()
