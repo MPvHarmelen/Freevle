788242bfb0221f2a86cfa19795a8b980b1f8d352
Martin van Harmelen <martin@vanharmelen.com>
2012-02-03 17:16:22 +0100

    Wrote letters.urls so the links in base.html work

--- letters/urls.py
+++ letters/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import *
 from django.views.generic.date_based import archive_index
+from django.views.generic.simple import redirect_to
 
 from cygy.letters.models import Letter
 
@@ -7,4 +8,5 @@ urlpatterns = patterns('',
     (r'^$', archive_index,
         {'queryset': Letter.objects.all(), 'date_field': 'publish',
          'num_latest': 30}),
+    (r'^(?P<slug>[-\w]*)/', redirect_to, {'url': '/media/letters/%(slug)s'})
 )
