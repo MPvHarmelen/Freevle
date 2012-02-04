02fe1e4271e81740344ec6d3aee92e0f8d241f11
Martin van Harmelen <Martin@vanharmelen.com>
2012-02-04 15:30:33 +0100

    Wrote photos.models
    I use an ImageField for the content, which requires the Python Imaging
    Library.

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Photo(models.Model):
+    title = models.CharField(max_length=32)
+    slug = models.SlugField()
+    publish = models.DateField(auto_now_add=True)
+    content = models.ImageField()
