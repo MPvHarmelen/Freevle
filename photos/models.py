37b07918deaa124eefe227f65f07dde35f536c9a
Martin van Harmelen <Martin@vanharmelen.com>
2012-02-04 21:42:12 +0100

    Added update_filename to photos.models

--- photos/models.py
+++ photos/models.py
@@ -2,7 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Photo(models.Model):
+    
+    def update_filename(instance, filename):
+        path = 'photos/'
+        new_file_name = instance.slug
+        extension = filename.split('.')[-1]
+        return path + new_file_name + '.' + extension
+    
     title = models.CharField(max_length=32)
     slug = models.SlugField()
     publish = models.DateField(auto_now_add=True)
-    content = models.ImageField()
+    content = models.ImageField(upload_to=update_filename)
