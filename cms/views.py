6f28145659c7fac6e79e81d79274c2969a850a52
Martin van Harmelen <Martin@vanharmelen.com>
2012-02-05 18:31:54 +0100

    Made cms.views code more understandable

--- cms/views.py
+++ cms/views.py
@@ -3,5 +3,5 @@ from django.views.generic.detail import DetailView
 
 class PageDetailView(DetailView):
     def get_queryset(self):
-        parent = self.kwargs.get('parent', None)
-        return self.model._default_manager.filter(parent__slug=parent)
+        parent_slug = self.kwargs.get('parent', None)
+        return self.model._default_manager.filter(parent__slug=parent_slug)
