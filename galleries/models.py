9cc55047ae87f2889f72d9acf91d8b212666aba3
Martin van Harmelen <Martin@vanharmelen.com>
2012-02-05 21:17:36 +0100

    Worked on galleries' views and models

--- galleries/models.py
+++ galleries/models.py
@@ -1,15 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class Gallery(models.Model):
+    name = models.CharField(max_length=32)
+    slug = models.SlugField(unique=True)
+    date = models.DateField()
+    
+    @models.permalink
+    def get_absolute_url(self):
+        pass
+
 class Photo(models.Model):
     
-    def update_filename(instance, filename):
-        path = 'photos/'
-        new_file_name = instance.slug
+    def update_filename(self, filename):
+        path = self.gallery.slug + '/photos/'
+        new_file_name = self.slug
         extension = filename.split('.')[-1]
         return path + new_file_name + '.' + extension
     
     title = models.CharField(max_length=32)
     slug = models.SlugField()
-    publish = models.DateField(auto_now_add=True)
+    gallery = models.ForeignKey(Gallery)
+    date = models.DateField()
     content = models.ImageField(upload_to=update_filename)
+    
+    @models.permalink
+    def get_absolute_url(self):
+        pass
--- galleries/views.py
+++ galleries/views.py
@@ -1,1 +1,7 @@
 # Create your views here.
+from django.views.generic.detail import DetailView
+
+class PhotoDetailView(DetailView):
+	def get_queryset(self):
+		gallery_slug = self.kwargs.get('gallery', None)
+		return self.model._default_manager.filter(gallery__slug=gallery_slug)
