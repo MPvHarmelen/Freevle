daf61420d2e3b4ad973db188e5c642f148321258
Martijn Arts <arts.martijn@gmail.com>
2012-02-06 14:50:53 +0100

    Finished auto-adding users.

--- users/management/__init__.py
+++ users/management/__init__.py
@@ -18,7 +18,8 @@ SURNAMES = ('Smith', 'Jones', 'Brown', 'Thompson', 'White', 'Scott', 'Coleman',
         'Hughes', 'Butler', 'McDonald', 'Shaw', 'Pond', 'Adams', 'Holmes',)
 DOMAINS = ('example', 'random', 'mydomain', 'internetforme', 'generic-isp',)
 TLDS = ('.net', '.com', '.org', '.me', '.pro', '.edu',)
-def create_user(group):
+
+def create_user(group, verbosity):
     first_name = random.choice(FIRST_NAMES)
     last_name = random.choice(SURNAMES)
     username = first_name[0] + last_name
@@ -31,22 +32,40 @@ def create_user(group):
     try:
         me.save()
     except IntegrityError:
-        me.username = me.username + str(random.randint(0, 99))
+        username = username + str(random.randint(0, 99))
+        me.username = username
         me.save()
     group.user_set.add(me)
 
+    if verbosity > 1:
+        print '- {}: "{} {}" <{}>'.format(username, first_name, last_name, email)
+
 def debug_data(sender, **kwargs):
+    verbosity = kwargs['verbosity']
+    if verbosity > 1:
+        print
+
     # Create 20 teachers
+    print ('Generating 20 random teachers (this may take a while)'
+           + (': ' if verbosity > 1 else '.'))
+
     teachers_group = models.Group.objects.get(name='Teachers') 
     for i in xrange(20):
-        create_user(teachers_group)
+        create_user(teachers_group, verbosity)
     teachers_group.save()
+    if verbosity > 1:
+        print
 
     # Create 100 students
+    print ('Generating 100 random students (this may take a while)'
+            + (': ' if verbosity > 1 else '.'))
+
     students_group = models.Group.objects.get(name='Students') 
     for i in xrange(100):
-        create_user(students_group)
+        create_user(students_group, verbosity)
     students_group.save()
+    if verbosity > 1:
+        print
 
 if DEBUG:
     post_syncdb.connect(debug_data, sender=models)
