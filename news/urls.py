62cdfbb3c5e1b6cf2892de5fb29fc2b2cf5740d7
Martijn Arts <arts.martijn@gmail.com>
2012-02-07 11:58:39 +0100

    Prettified news.urls a teeny bit.

--- news/urls.py
+++ news/urls.py
@@ -8,22 +8,20 @@ from cygy.news.models import NewsMessage
 urlpatterns = patterns('',
     url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[-\w]*)/$',
         DateDetailView.as_view(
-            queryset=NewsMessage.objects.all(), date_field='publish',
-            month_format='%m', context_object_name='message'),
+            model=NewsMessage, date_field='publish', month_format='%m',
+            context_object_name='message'),
         name='newsmessage-detail'),
 
     url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/$',
         DayArchiveView.as_view(
-            queryset=NewsMessage.objects.all(), date_field='publish',
-            paginate_by=10, month_format='%m', allow_empty=True,
-            context_object_name='message'),
+            model=NewsMessage, date_field='publish', paginate_by=10,
+            month_format='%m', allow_empty=True, context_object_name='message'),
         name='newsmessage-day'),
 
     url(r'^(?P<year>\d{4})/(?P<month>\d{2})/$',
         MonthArchiveView.as_view(
-            queryset=NewsMessage.objects.all(), date_field='publish',
-            paginate_by=10, month_format='%m', allow_empty=True,
-            context_object_name='message'),
+            model=NewsMessage, date_field='publish', paginate_by=10,
+            month_format='%m', allow_empty=True, context_object_name='message'),
         name='newsmessage-month'),
 
     url(r'^$',
