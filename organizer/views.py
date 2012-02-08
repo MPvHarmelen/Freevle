59580e78c5727367df543114f80fcfa17276f0f8
Martin van Harmelen <Martin@vanharmelen.com>
2012-02-08 21:10:53 +0100

    Wrote organizer.views.UserView

--- organizer/views.py
+++ organizer/views.py
@@ -1,8 +1,36 @@
-from django.shortcuts import render_to_response
-import datetime
-import organizer.models
+from django.views.generic.detail import DetailView
 
 # Create your views here.
-def now(request):
-    pass
+class UserView(DetailView):
+    username_field = 'username'
+    username_url_kwarg = 'username'
 
+    def get_object(self, queryset=None):
+        
+
+        # Use a custom queryset if provided; this is required for subclasses
+        # like DateDetailView
+        if queryset is None:
+            queryset = self.get_queryset()
+
+        username = self.kwargs.get(self.username_url_kwarg, None)
+
+        # Try looking up by username.
+        if username is not None:
+            username_field = self.username_field
+            queryset = queryset.filter(**{username_field: username})
+
+        # If none of those are defined, it's an error.
+        else:
+            raise AttributeError(u'Generic detail view {} must be called with '
+                                 u'a username.'.format(self.__class__.__name__))
+
+        try:
+            obj = queryset.get()
+        except ObjectDoesNotExist:
+            raise Http404(
+                _(u'No {verbose_name} found matching the query').format(
+                    verbose_name=queryset.model._meta.verbose_name
+                    )
+                )
+        return obj
