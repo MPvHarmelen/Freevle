52d4c8291df1143f67845b4eefd68d8b0ff6d437
Martin van Harmelen <Martin@vanharmelen.com>
2012-02-10 18:15:37 +0100

    Removed line# in organizer.views

--- organizer/views.py
+++ organizer/views.py
@@ -22,29 +22,29 @@ class UserView(TemplateResponseMixin, View):
 
 
         # To Do
-        template_name_suffix = '_list'
-	
-	    def get_template_names(self):
-127	        """
-128	        Return a list of template names to be used for the request. Must return
-129	        a list. May not be called if get_template is overridden.
-130	        """
-131	        try:
-132	            names = super(MultipleObjectTemplateResponseMixin, self).get_template_names()
-133	        except ImproperlyConfigured:
-134	            # If template_name isn't specified, it's not a problem --
-135	            # we just start with an empty list.
-136	            names = []
-137	
-138	        # If the list is a queryset, we'll invent a template name based on the
-139	        # app and model name. This name gets put at the end of the template
-140	        # name list so that user-supplied names override the automatically-
-141	        # generated ones.
-142	        if hasattr(self.object_list, 'model'):
-143	            opts = self.object_list.model._meta
-144	            names.append("%s/%s%s.html" % (opts.app_label, opts.object_name.lower(), self.template_name_suffix))
-145	
-146	        return names
+    template_name_suffix = '_list'
+
+    def get_template_names(self):
+        """
+        Return a list of template names to be used for the request. Must return
+        a list. May not be called if get_template is overridden.
+        """
+        try:
+            names = super(MultipleObjectTemplateResponseMixin, self).get_template_names()
+        except ImproperlyConfigured:
+            # If template_name isn't specified, it's not a problem --
+            # we just start with an empty list.
+            names = []
+
+        # If the list is a queryset, we'll invent a template name based on the
+        # app and model name. This name gets put at the end of the template
+        # name list so that user-supplied names override the automatically-
+        # generated ones.
+        if hasattr(self.object_list, 'model'):
+            opts = self.object_list.model._meta
+            names.append("%s/%s%s.html" % (opts.app_label, opts.object_name.lower(), self.template_name_suffix))
+
+            return names
 
 
 
