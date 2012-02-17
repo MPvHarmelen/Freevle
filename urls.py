7f8da0f15fb7f690d54138616f836dc8b6cb70fc
Martijn Arts <arts.martijn@gmail.com>
2012-02-17 12:27:00 +0100

    Added a direct_to_template for courses for Floris.

--- urls.py
+++ urls.py
@@ -22,6 +22,8 @@ urlpatterns += patterns('',
     url(r'^news/', include('cygy.news.urls')),
     url(r'^letters/', include('cygy.letters.urls')),
     url(r'^galleries/', include('cygy.galleries.urls')),
+    # TEMP for Floris' work.
+    url(r'^courses/', direct_to_template, {'template': 'roosters/index.html'}),
 
     # User urls
     url(r'^user/login/$', 'django.contrib.auth.views.login', {'template_name': 'user/login.html'}),
