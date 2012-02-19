917874df4f05bdf2c54039ea0b525f1916283c5f
Martin van Harmelen <Martin@vanharmelen.com>
2012-02-19 22:11:25 +0100

    Cleaned up organizer.views

--- organizer/urls.py
+++ organizer/urls.py
@@ -1,8 +1,6 @@
 from django.conf.urls.defaults import *
 from cygy.organizer.views import UserView
 
-from django.contrib.auth.models import User
-
 urlpatterns = patterns('',
 	url(r'^(?P<username>[\w-]+)/$', UserView.as_view(template_name='organizer/my_schedule.html'),
 	 	name='organizer-user'),
