2eb79034def63578a821ac2b3554410bf34f3d0c
Martin van Harmelen <Martin@vanharmelen.com>
2012-02-22 21:51:08 +0100

    Renamed custom.classes.my_list to sortable_list

--- custom/classes.py
+++ custom/classes.py
@@ -1,6 +1,6 @@
 class sortable_list(list):
     def sorted(self):
         # This next line scares the hell out of me O_o
-        li = my_list(self)
+        li = sortable_list(self)
         li.sort()
         return li
