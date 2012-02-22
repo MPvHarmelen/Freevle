dfe7c1aa0f9196aa67246a339ae2385ec108e8f6
Martin van Harmelen <Martin@vanharmelen.com>
2012-02-22 22:02:04 +0100

    Put args kwargs shit in custom.sortable_list

--- custom/classes.py
+++ custom/classes.py
@@ -1,6 +1,6 @@
 class sortable_list(list):
-    def sorted(self):
+    def sorted(self, *args, **kwargs):
         # This next line scares the hell out of me O_o
         li = sortable_list(self)
-        li.sort()
+        li.sort(*args, **kwargs)
         return li
