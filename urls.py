d98414526c85a0004496c91d01e6242ed702045b
Floris Jansen <florismartijnjansen@gmail.com>
2012-03-23 21:42:43 +0100

    add photo-page

--- urls.py
+++ urls.py
@@ -28,7 +28,7 @@ urlpatterns += patterns('',
     url(r'^courses/', TemplateView.as_view(template_name='courses/index.html')),
     url(r'^contact/', TemplateView.as_view(template_name='contact/index.html')),
     url(r'^settings/', TemplateView.as_view(template_name='settings/index.html')),
-
+    url(r'^photos/', TemplateView.as_view(template_name='photos/index.html')),
 
     # User urls
     url(r'^user/login/$', 'django.contrib.auth.views.login', {'template_name': 'user/login.html'}),
