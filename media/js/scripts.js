599c4a8b3aaa72a6bde15d92506dcc675c14ef69
Floris Jansen <florismartijnjansen@gmail.com>
2012-03-23 23:20:02 +0100

    add ctrl+s functionality

--- media/js/scripts.js
+++ media/js/scripts.js
@@ -125,6 +125,20 @@ $(document).ready(function(){
   }
 });
 
+//Ctrl+
+$.ctrl = function(key, callback, args) {
+    $(document).keydown(function(e) {
+        if(!args) args=[];
+        if(e.keyCode == key.charCodeAt(0) && e.ctrlKey) {
+            callback.apply(this, args);
+            return false;
+        }
+    });
+};
+$.ctrl('S', function() {
+  $('.course').trigger('click');
+});
+
 document.createElement('header');
 document.createElement('footer');
 
