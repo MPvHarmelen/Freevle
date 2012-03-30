6e0efd7ddd03c449e3cbd03d1bb2c833e3aeda11
Martijn Arts <arts.martijn@gmail.com>
2012-03-30 21:34:55 +0200

    Last fixes for renaming to Schoolr.

--- cms/views.py
+++ cms/views.py
@@ -10,8 +10,3 @@ class PageDetailView(DetailView):
     def get_queryset(self):
         parent_slug = self.kwargs.get('parent', None)
         return self.model._default_manager.filter(parent__slug=parent_slug)
-
-def index(request)
-    now = datetime.now()
-    last_news = NewsMessage.objects.filter(publish__lte=now).order_by('-publish')[0]
-    #return render_to_response('index.html', 
