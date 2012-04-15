77739eada2da69e2596c8519950562abf9702044
Martin van Harmelen <Martin@vanharmelen.com>
2012-04-15 18:41:41 +0200

    Corrected mistake in cms.models

--- freevle/cms/models.py
+++ freevle/cms/models.py
@@ -9,18 +9,18 @@ class Page(models.Model):
     title = models.CharField(max_length=18)
     slug = models.SlugField(
 #        unique=True,
-        help_text=_('URL-friendly version of the title,\
-                    can be left alone most of the time.')
+        help_text=_('URL-friendly version of the title, '
+                    'can be left alone most of the time.')
     )
     content = models.TextField(help_text=_('Content of the page.'))
-    
+
     def save(self, *args, **kwargs):
         sibling_slugs = [sibling.slug for sibling in Page.objects.filter(parent=self.parent)]
         if self.slug in sibling_slugs:
             raise IntegrityError(_("There's already a page with this parent and slug"))
         else:
             super(Page, self).save(*args, **kwargs)
-    
+
     @models.permalink
     def get_absolute_url(self):
         if self.parent:
@@ -32,7 +32,7 @@ class Page(models.Model):
             return ('cms-parent-detail', (), {
                 'slug':self.slug,
             })
-    
+
     def __unicode__(self):
         return self.title
 
