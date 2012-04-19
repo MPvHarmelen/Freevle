e797a92dbaf5c99d3d08ada2e4059327136fe66f
Martin van Harmelen <Martin@vanharmelen.com>
2012-04-19 22:19:04 +0200

    Started working on admin app_list context processor

--- freevle/custom/context_processors.py
+++ freevle/custom/context_processors.py
@@ -1,3 +1,7 @@
 def cms(request):
     from freevle.cms.models import Page
     return {'pages':Page.objects.all()}
+
+def  app_list(request):
+    if request.path.split('/')[0] == 'admin':
+        pass
