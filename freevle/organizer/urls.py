a41ab7833d1dd9052fa435a83b5bce128ec9d4e1
Martijn Arts <arts.martijn@gmail.com>
2012-04-23 13:00:08 +0200

    Fixed some urls to accept digits for usernames.

--- freevle/organizer/urls.py
+++ freevle/organizer/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls.defaults import *
 from freevle.organizer.views import StudentView
 
 urlpatterns = patterns('',
-    url(r'^(?P<username>[\w-]+)/$', StudentView.as_view(template_name='organizer/my_schedule.html'),
+    url(r'^(?P<username>[\d\w-]+)/$', StudentView.as_view(template_name='organizer/my_schedule.html'),
         name='organizer-user'),
-    url(r'^(?P<username>[\w-]+)/(?P<day>\d{2})/(?P<month>\d{2})/$', StudentView.as_view(template_name='organizer/my_schedule.html'),
+    url(r'^(?P<username>[\d\w-]+)/(?P<day>\d{2})/(?P<month>\d{2})/$', StudentView.as_view(template_name='organizer/my_schedule.html'),
         name='organizer-user-day'),
 ) 
