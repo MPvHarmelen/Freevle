b3311e348b18527681bbef9730f2251326a74790
Martijn Arts <arts.martijn@gmail.com>
2012-04-24 15:00:13 +0200

    Made Change Password in settings work.

--- freevle/users/urls.py
+++ freevle/users/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls.defaults import *
 from django.views.generic import TemplateView
 
-urlpatterns = patterns('',
-    url(r'^(?P<page>\w*/?)$', TemplateView.as_view(template_name='user/settings/index.html')),
-)
+from freevle.users.views import *
 
+urlpatterns = patterns('',
+    url(r'^changepassword/$', changepassword),
+    url(r'^(?P<page>\w*/?)$',
+        TemplateView.as_view(template_name='user/settings.html')),
+)
