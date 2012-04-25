e73ce2dc5d0c8fa08b7ba6194c884b401fbfeabd
Floris Jansen <florismartijnjansen@gmail.com>
2012-04-25 21:13:32 +0200

    add autocompletion, array is dummy and results don't help yet

---
+++ freevle/media/js/autocompletion.js
@@ -0,0 +1,68 @@
+$(document).ready(function() {
+  var rearea = $('#searchresults');
+  var searchfield = $('#search-box');
+  var results = ['aaaaa', 'aaaab', 'bbbbb'];
+
+  $('.resul').live('click', function(){
+      $('.activeres').removeClass('activeres');
+      $(this).addClass('activeres');
+      searchfield.focus();
+  });
+
+  rearea.hide();
+
+  searchfield.focus(function() {
+      var chars = $(this).val();
+      if(chars.length >= 2) {
+          rearea.stop(true, true).slideDown('fast');
+      }
+  });
+  searchfield.blur(function() {
+      rearea.stop(true, true).slideUp('fast');
+  });
+  searchfield.keydown(function(e) {
+      if (e.keyCode == 38 || e.keyCode == 40) {
+          var pos = this.selectionStart;
+          this.selectionStart = pos; this.selectionEnd = pos;
+          e.preventDefault();
+      }
+  });
+
+
+  searchfield.keyup(function(e) {
+      if(e.keyCode == 40) {
+          if($('.resul:last').hasClass('activeres')) {
+              $('.activeres').removeClass('activeres');
+              rearea.children('li:first').addClass('activeres');
+          } else {
+              $('.activeres').removeClass('activeres').next('li').addClass('activeres');
+          }
+      } else if(e.keyCode == 38) {
+          if(rearea.children('li:first').hasClass('activeres')) {
+              $('.activeres').removeClass('activeres');
+              rearea.children('li:last').addClass('activeres');
+          } else {
+              $('.activeres').removeClass('activeres').prev('li').addClass('activeres');
+          }
+      } else if(e.keyCode == 37 || e.keyCode == 39 || e.keyCode == 16 || e.keyCode == 17 || e.keyCode == 18 || e.keyCode == 19 || e.keyCode == 20 || e.keyCode == 35 || e.keyCode == 36 || e.keyCode == 91 || e.keyCode == 92) {
+
+      } else if(e.keyCode == 13) {
+          //Select this one
+
+      } else {
+          var chars = $(this).val();
+          if(chars.length >= 2) {
+              rearea.html('');
+              rearea.append('<li class="activeres"></li>');
+              for(var i = 0; i < results.length; i++) {
+                  result = results[i];
+                  result = result.replace(chars, '<strong>' + chars + '</strong>');
+                  rearea.append('<li class="resul">' + result + '</li>');
+              }
+              rearea.slideDown('fast');
+          } else {
+              rearea.slideUp('fast');        
+          }
+      }
+  });
+});
