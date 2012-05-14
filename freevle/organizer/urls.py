0fe193068203a85621bdfdcad42e37da2877781b
Martin van Harmelen <Martin@vanharmelen.com>
2012-05-14 21:21:47 +0200

    Renamed organizer template

--- freevle/organizer/urls.py
+++ freevle/organizer/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls.defaults import *
 from freevle.organizer.views import organizer_view
 
 urlpatterns = patterns('',
-    url(r'^(?P<slug>[\d\w-]+)/$', organizer_view, {'template_name':'organizer/my_schedule.html'},
+    url(r'^(?P<slug>[\d\w-]+)/$', organizer_view, {'template_name':'organizer/student_organizer.html'},
         name='organizer'),
-    url(r'^(?P<slug>[\d\w-]+)/(?P<day>\d{2})-(?P<month>\d{2})/$', organizer_view, {'template_name':'organizer/my_schedule.html'},
+    url(r'^(?P<slug>[\d\w-]+)/(?P<day>\d{2})-(?P<month>\d{2})/$', organizer_view, {'template_name':'organizer/student_organizer.html'},
         name='organizer-date'),
 )
