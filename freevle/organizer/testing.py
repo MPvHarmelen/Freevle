3564e3a36fcaf234c6bc7ed7b96117c412e7d55b
Martin van Harmelen <Martin@vanharmelen.com>
2012-07-04 17:23:24 +0200

    Corrected due_date in organizer.testing

--- freevle/organizer/testing.py
+++ freevle/organizer/testing.py
@@ -7,10 +7,12 @@ def make_homework(course_abbr=None, homeworktype_abbr=None, date_format=None, da
     content = content or input('Homework content: ')
     date_format = date_format or '%d-%m-%Y'
     date_str = date_str or input('Date({}): '.format(date_format))
+    due_date = datetime.strptime(date_str, date_format)
     for lesson in Lesson.objects.filter(course__topic__abbr=course_abbr):
-        Homework(
-            homework_type=HomeworkType.objects.get(abbr=homeworktype_abbr),
-            lesson=lesson,
-            content=content,
-            due_date=datetime.strptime(date_str, date_format)
-        ).save()
+        if lesson.day_of_week == due_date.strftime('%a'):
+            Homework(
+                homework_type=HomeworkType.objects.get(abbr=homeworktype_abbr),
+                lesson=lesson,
+                content=content,
+                due_date=due_date
+            ).save()
