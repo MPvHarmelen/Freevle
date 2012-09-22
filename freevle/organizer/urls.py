6148d301605d28732cd181264f5d92a1bd19cc56
Martin van Harmelen <Martin@vanharmelen.com>
2012-09-22 17:31:49 +0200

    Changed organizer.urls to get years right

--- freevle/organizer/urls.py
+++ freevle/organizer/urls.py
@@ -4,6 +4,6 @@ from freevle.organizer.views import organizer_view
 urlpatterns = patterns('',
     url(r'^(?P<slug>[\d\w-]+)/$', organizer_view, {},
         name='organizer'),
-    url(r'^(?P<slug>[\d\w-]+)/(?P<day>\d{2})-(?P<month>\d{2})/$', organizer_view, {},
+    url(r'^(?P<slug>[\d\w-]+)/(?P<day>\d{2})-(?P<month>\d{2})-(?P<year>\d{4})/$', organizer_view, {},
         name='organizer-date'),
 )
