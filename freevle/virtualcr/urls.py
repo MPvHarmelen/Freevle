ae3e2ed29ecfa0185ec0493104ab487ad90f2e11
Martijn Arts <arts.martijn@gmail.com>
2012-09-23 00:37:06 +0200

    Changed urls to account for plugins

--- freevle/virtualcr/urls.py
+++ freevle/virtualcr/urls.py
@@ -2,12 +2,20 @@ from django.conf.urls.defaults import *
 from django.views.generic.detail import DetailView
 
 from freevle.virtualcr.models import VirtualClassroom
+from freevle.virtualcr import plugins
 
-urlpatterns = patterns('',
-    url(r'^(?P<slug>[-\w]*)', DetailView.as_view(
-            model=VirtualClassroom,
-            context_object_name='virtualcr'
-        ),
-        name='virtualcr-detail')
-)
+urls = ['']
 
+# Include urls for plugins
+for plugin in plugins.get_plugins():
+    urls.append(url(r'^' + plugin[0] + '/', include(plugin[1] + '.urls')))
+
+urls.append(
+        url(r'^(?P<slug>[-\w]*)', DetailView.as_view(
+                model=VirtualClassroom,
+                context_object_name='virtualcr'
+            ),
+            name='virtualcr-detail')
+    )
+
+urlpatterns = patterns(*urls)
