5599390e8d2c0452f35e02cc3e7d7c9ad6642cb6
Martijn Arts <arts.martijn@gmail.com>
2012-09-22 17:39:23 +0200

    Fixed Martin's TABS fuckup...

--- freevle/galleries/admin.py
+++ freevle/galleries/admin.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 from freevle.galleries.models import Gallery, Photo
 
 class GalleryAdmin(admin.ModelAdmin):
-	prepopulated_fields = {'slug': ('name',)}
-	list_display = ('name','date')
+    prepopulated_fields = {'slug': ('name',)}
+    list_display = ('name','date')
 
 class PhotoAdmin(admin.ModelAdmin):
-	prepopulated_fields = {'slug': ('title',)}
-	list_display = ('title',)
+    prepopulated_fields = {'slug': ('title',)}
+    list_display = ('title',)
 
 admin.site.register(Gallery,GalleryAdmin)
 admin.site.register(Photo,PhotoAdmin)
