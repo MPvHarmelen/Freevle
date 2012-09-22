040159db04366f6f480b951a90d36de8dbafb0dd
Martijn Arts <arts.martijn@gmail.com>
2012-09-22 17:28:45 +0200

    Removed FUCKING TRAILING SPACES >:(

--- freevle/galleries/models.py
+++ freevle/galleries/models.py
@@ -5,31 +5,30 @@ class Gallery(models.Model):
     name = models.CharField(max_length=32)
     slug = models.SlugField(unique=True)
     date = models.DateField()
-    
+
     def __unicode__(self):
         return self.name
-    
+
     @models.permalink
     def get_absolute_url(self):
         pass
 
 class Photo(models.Model):
-    
     def _update_filename(self, filename):
         path = self.gallery.slug + '/photos/'
         new_file_name = self.slug
         extension = filename.split('.')[-1]
         return path + new_file_name + '.' + extension
-    
+
     title = models.CharField(max_length=32)
     slug = models.SlugField(unique=True)
     gallery = models.ForeignKey(Gallery)
     date = models.DateField()
     content = models.ImageField(upload_to=_update_filename)
-    
+
     def __unicode__(self):
         return self.title
-    
+
     @models.permalink
     def get_absolute_url(self):
         pass
