d878b902953576cf512ece1d3b6da5ec1da26a7d
Martin van Harmelen <Martin@vanharmelen.com>
2012-12-03 16:29:02 +0100

    Finished gallaries #14

--- freevle/galleries/urls.py
+++ freevle/galleries/urls.py
@@ -4,7 +4,7 @@ from django.views.generic.detail import DetailView
 from freevle.galleries.models import Gallery
 
 urlpatterns = patterns('',
-    url(r'^(?P<slug>[-\w]*)$', DetailView.as_view(model=Gallery),
+    url(r'^(?P<slug>[-\w]*)/$', DetailView.as_view(model=Gallery),
         name='gallery-detail')
 #    url(r'^(?P<gallery>[-\w]*)#(?P<slug>[-\w]*)', )
 )
