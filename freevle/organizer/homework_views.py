bc488599000d89b2fa285eed6ba73dc72d2999ce
Martin van Harmelen <Martin@vanharmelen.com>
2013-06-01 22:01:21 +0200

    Saving homework works too #49
    Only the error feedback to go!

--- freevle/organizer/homework_views.py
+++ freevle/organizer/homework_views.py
@@ -17,7 +17,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.core.urlresolvers import reverse
-from django.core.exceptions import PermissionDenied
+from django.core.exceptions import PermissionDenied, ValidationError
 
 @login_required
 def get_course_view(request):
@@ -65,19 +65,20 @@ def get_next_lessons(course, last_date):
 
     return new_date, sorted(lessons, key=lambda a:a.period)
 
-def get_empty_homework(lesson, date, content=''):
+def get_empty_homework(course, date, content='', period=None):
     '''
     Returns a Homework object without a HomeworkType. Because the HomeworkType
     is in the name of a Homework object, you can't an 'empty' Homework.
     '''
     return Homework(
-        content = '',
-        lesson = lesson,
-        due_date = date
+        content='',
+        course=course,
+        due_date=date,
+        period=period
     )
 
 @login_required
-def update_homework_view(request, slug=None, minimum_homework=10):
+def update_homework_view(request, slug=None, minimum_homework=10, date_format='%d-%m-%Y', homework_list=None):
     if slug is None:
         raise ImproperlyConfigured('update_homework_view should be called with '
                                    'a slug.')
@@ -86,7 +87,58 @@ def update_homework_view(request, slug=None, minimum_homework=10):
     if course in request.user.gives_courses.all():
         if request.method == 'POST':
             # First save and validate all that shit.
-            print request.POST
+            ids = request.POST.getlist('id')
+            hw_types = request.POST.getlist('homework_type')
+            contents = request.POST.getlist('content')
+            due_dates = request.POST.getlist('due_date')
+            periods = request.POST.getlist('period')
+            homework_list = []
+            due_dateles_homework_list = []
+            for i, id in enumerate(ids):
+                if periods[i] == 'None':
+                    periods[i] = None
+                if id == 'None':
+                    id = None
+                try:
+                    homework = Homework.objects.get(id=id)
+                except Homework.DoesNotExist:
+                    homework = Homework(course=course)
+                else:
+                    if homework.course != course:
+                        # This means someone inserted an id in the html
+                        # and because they aren't allowed to edit other
+                        # homework than for this course:
+                        raise PermissionDenied()
+
+                homework.content = contents[i]
+                homework.period = periods[i]
+                homework.homework_type_id = id=hw_types[i]
+
+                errors = {}
+                try:
+                    homework.due_date = datetime.datetime.strptime(due_dates[i], date_format).date()
+                except ValueError as e:
+                    errors.update(due_date=_('Date not recognised, correct format: {}'.format(date_format)))
+
+                try:
+                    homework.full_clean()
+                except ValidationError as e:
+                    for arg, messages in e.message_dict.items():
+                        errors[arg] = [_(m) for m in messages]
+
+                if errors == {}:
+                    homework.save()
+                    print 'saved'
+                    # This has to be verbal because the template language sucks.
+                    homework.saved = 'yes'
+                else:
+                    homework.saved = 'no'
+
+                homework.errors = errors
+                homework_list.append(homework)
+
+            # This has to be fed to the template or something...
+            homework_list.sort(key=lambda a: (a.due_date, a.period))
 
             # Then check where to go.
             course_slug = request.POST.get('course') or course.slug
@@ -95,20 +147,23 @@ def update_homework_view(request, slug=None, minimum_homework=10):
             return HttpResponseRedirect(reverse(url, kwargs=kwargs))
 
         else:
-            homework_list = []
-            # get_next_lessons only does dates AFTER `date`, so `date` must be
-            # yesterday to include today.
-            prev_date = datetime.date.today() - datetime.timedelta(days=1)
-            while len(homework_list) < minimum_homework:
-                date, lessons = get_next_lessons(course, prev_date)
-                for lesson in lessons:
-                    homework = lesson.homework_set.filter(due_date__gt=prev_date,
-                                                          due_date__lte=date)
+            if homework_list is None:
+                homework_list = []
+                # get_next_lessons only does dates AFTER `date`, so `date` must be
+                # yesterday to include today.
+                prev_date = datetime.date.today() - datetime.timedelta(days=1)
+                while len(homework_list) < minimum_homework:
+                    date, lessons = get_next_lessons(course, prev_date)
+                    homework = course.homework_set.filter(
+                        due_date__gt=prev_date,
+                        due_date__lte=date
+                    )
                     homework_list.extend(homework)
-                    if len(homework.filter(due_date=date)) == 0:
-                        homework_list.append(get_empty_homework(lesson, date))
-                prev_date = date
-            homework_list.sort(key=lambda a: a.due_date)
+                    for lesson in lessons:
+                        if len(homework.filter(due_date=date, period=lesson.period)) == 0:
+                            homework_list.append(get_empty_homework(course, date, period=lesson.period))
+                    prev_date = date
+                homework_list.sort(key=lambda a: (a.due_date, a.period))
 
             # Template variables
             homework_types = HomeworkType.objects.all()
