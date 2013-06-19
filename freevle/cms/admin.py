085fd2004cf322ffd9229fa06eb8ec21a284d4c3
Martin van Harmelen <Martin@vanharmelen.com>
2013-06-19 11:01:33 +0200

    Replaced " by ' in cms.admin because of my ocd

--- freevle/cms/admin.py
+++ freevle/cms/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from freevle.cms.models import Page
 
 class PageAdmin(admin.ModelAdmin):
-    prepopulated_fields = {"slug": ("title",)}
+    prepopulated_fields = {'slug': ('title',)}
     list_display = ('title', 'parent',)
 
 
