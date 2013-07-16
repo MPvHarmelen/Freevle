9e0a5756ab0271a04b67ca76b9af4dd9985c6ae1
Martin van Harmelen <Martin@vanharmelen.com>
2013-07-16 18:17:45 +0200

    Wrote first cms model

--- freevle/apps/cms/models.py
+++ freevle/apps/cms/models.py
@@ -1,4 +1,12 @@
 from freevle import db
 
 class Page(db.Model):
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    parent = db.relationship('Page',
+                             backref=db.backref('children', lazy='dynamic'))
+    title = db.Column(db.String(32))
+    slug = db.Column(db.String(32))
+    content = db.Column(db.Text)
+    last_edited = db.Column(db.DateTime)
+    __table_args__ = (db.UniqueConstraint('parent', 'slug', name='_parent_slug_uc'))
+
