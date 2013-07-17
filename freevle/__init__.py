7ab3147c55a65add119fba9eae68ee691c17ee16
Martijn Arts <arts.martijn@gmail.com>
2013-07-17 11:01:36 +0200

    Ironed out last bugs: now runs! Added config file.

--- freevle/__init__.py
+++ freevle/__init__.py
@@ -9,6 +9,7 @@ from flask.ext.seasurf import SeaSurf
 app = Flask(__name__)
 # Load settings (defaults from file, others from wherever user wants)
 app.config.from_object('freevle.default_settings')
+app.config.from_envvar('FREEVLE_SETTINGS')
 
 # Initialize Flask extensions
 db = SQLAlchemy(app)
@@ -16,7 +17,7 @@ babel = Babel(app)
 csrf = SeaSurf(app)
 
 # Find and import blueprints.
-blueprints = os.listdir('apps')
+blueprints = os.listdir(app.config['APPS_DIRECTORY'])
 for bp_name in blueprints:
-    bp = importlib.import_module('apps.' + bp_name)
+    bp = importlib.import_module('freevle.apps.' + bp_name)
     app.register_blueprint(bp.mod, url_prefix=bp.URL_PREFIX)
