b10ccce8b2917f899fd794a470eb493d6377abeb
Martin van Harmelen <Martin@vanharmelen.com>
2013-07-17 14:44:59 +0200

    Wrote wireframe for most bps
    Difficult blueprints are:
    - cms (not)
    - importer
    - organizer
    - user
    - virtualcr

--- freevle/apps/cms/views.py
+++ freevle/apps/cms/views.py
@@ -2,4 +2,26 @@ from . import bp
 
 @bp.route('/')
 def home():
-    """The homepage of the entire website."""
+    """Show the homepage of the entire website."""
+
+@bp.route('/<page_slug>')
+@bp.route('/<parent_slug>/<page_slug>')
+def page_view(page_slug, parent_slug=None):
+    """Show a page from the database."""
+
+# Admin
+@bp.route('/create')
+@bp.route('/<parent_slug>/create')
+@bp.route('/<page_slug>/edit')
+@bp.route('/<parent_slug>/<page_slug>/edit')
+def page_edit(page_slug=None, parent_slug=None):
+    """Create or edit a page."""
+    if page_slug is None:
+        # First or second routing, create a page.
+    else:
+        # Third or fourth routing, edit a page.
+
+@bp.route('/<page_slug>/delete')
+@bp.route('/<parent_slug>/<page_slug>/delete')
+def page_delete(page_slug, parent_slug=None):
+    """Delete a page."""
--- freevle/apps/virtualcr/views.py
+++ freevle/apps/virtualcr/views.py
@@ -1,1 +1,24 @@
 from . import bp
+
+@bp.route('/')
+def virtualcr_list_view():
+    """Show a list of all VirtualCRs."""
+
+@bp.route('/<virtualcr_slug>/')
+def virtualcr_view(virtualcr_slug):
+    """The overview of a VirtualCR with all its nodes in it."""
+
+# TODO: Think of how these urls should work with sections and nodes and ahhh.
+#@bp.route('/<vritual_slug>/<
+
+# Admin
+@bp.route('/create')
+@bp.route('/<virtualcr_slug>/edit')
+def virtualcr_edit(virtualcr_slug=None):
+    """Create or edit a VirtualCR"""
+
+@bp.route('/<virtualcr_slug>/delete')
+def virtualcr_delete(virtualcr_slug):
+    """Delete a VirtualCR."""
+
+#TODO: Route CED view for section and node
