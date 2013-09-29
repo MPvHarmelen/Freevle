d6ffb5fac0ed329a962a19b6b040c9537ad03ceb
Floris Jansen <florismartijnjansen@gmail.com>
2013-09-29 23:49:17 +0200

    add schoolklimaat-page

--- freevle/tmp-design/app.py
+++ freevle/tmp-design/app.py
@@ -4,13 +4,18 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     title = 'Cygnus Gymnasium'
-    return render_template('index.html', title=title, index=True)
+    return render_template('index.html', title=title)
 
 @app.route('/<name>')
 def page(name):
     title = name.capitalize() + ' | Cygnus Gymnasium'
     page = name + '.html'
-    return render_template(page, title=title, index=True)
+    return render_template(page, title=title)
+
+@app.route('/over/<a>/<b>')
+def aboutpage(a,b):
+    title = 'Schoolklimaat | Cygnus Gymnasium'
+    return render_template('schoolklimaat.html', title=title)
 
 if __name__ == '__main__':
     app.debug = True
