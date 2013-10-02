c72d9621b90b51849fd4f9641a47410c96ee2a00
Martin van Harmelen <Martin@vanharmelen.com>
2013-10-02 13:41:39 +0200

    Wrote cms models and updated views and tests
    - Made Category
    - Made Subcategory
    - Made PageSection
    - Made TextSection
    - Made ImageSection

--- freevle/blueprints/cms/views.py
+++ freevle/blueprints/cms/views.py
@@ -1,11 +1,22 @@
 from . import bp
-from .models import Page
-from flask import g, render_template, Markup
+from .models import Category, Subcategory, Page, PageSection
+from flask import render_template, Markup, request
+from markdown import Markdown
+
+markdown = Markdown(output_format='html5', safe_mode='escape').convert
 
 @bp.context_processor
 def inject_sitemap():
-    parents = Page.query.filter(Page.parent_id==0)
-    return dict(parents=parents)
+    """Inject categories into context."""
+    categories = Category.query.all()
+    # TODO: where does 'contact' get in?
+    return dict(categories=categories)
+
+@bp.context_processor
+def inject_breadcrumbs():
+    """Inject breadcrumbs extracted from url into context."""
+    #request.url
+    return dict()
 
 @bp.route('/')
 def home():
@@ -13,32 +24,55 @@ def home():
     # TODO: make this work for: Page.parent == None
     return render_template('cms/index.html')
 
-@bp.route('/<page_slug>/')
-@bp.route('/<parent_slug>/<page_slug>/')
-def page_view(page_slug, parent_slug=None):
+@bp.route('/<category_slug>/')
+def category_view(category_slug):
+    """Show an overview of a category, with squares for subcategories."""
+    category = Category.query.filter(Category.slug == category_slug).first_or_404()
+    return render_template('cms/category_view.html', category=category)
+
+@bp.route('/<category_slug>/<subcategory_slug>/<page_slug>')
+def page_view(category_slug, subcategory_slug, page_slug):
     """Show a page from the database."""
-    possible_parent_ids = [p.id for p in Page.query.filter(Page.slug == parent_slug).all()] or [0]
-    page = Page.query.filter(Page.slug == page_slug).filter(Page.parent_id.in_(possible_parent_ids)).first_or_404()
-    page.content = Markup(page.content)
+    # # TODO: find out if this could be put into one query.
+    cat = Category.query.filter(Category.slug == category_slug).first_or_404()
+    sub = Subcategory.query.filter(Subcategory.category == cat)\
+          .filter(Subcategory.slug == subcategory_slug).first_or_404()
+    page = Page.query.filter(Page.subcategory == sub)\
+           .filter(Page.slug == page_slug).first_or_404()
+
+    for text_section in page.sections.filter(PageSection.section_type == 'text'):
+        text_section.content = Markup(markdown(text_section.content))
     return render_template('cms/page_view.html', page=page)
 
 
 # Admin
-@bp.route('/create')
-@bp.route('/<parent_slug>/create')
-@bp.route('/<page_slug>/edit')
-@bp.route('/<parent_slug>/<page_slug>/edit')
+# This should change a lot because Floris and Pim want an admin site
+@bp.route('/admin/')
+def admin_index():
+    """Site wide admin homepage."""
+    ...
+
+@bp.route('/admin/cms/')
+def admin():
+    """Specific admin index for cms blueprint."""
+    ...
+
+
+# @bp.route('/create')
+# @bp.route('/<parent_slug>/create')
+# @bp.route('/<page_slug>/edit')
+# @bp.route('/<parent_slug>/<page_slug>/edit')
 def page_edit(page_slug=None, parent_slug=None):
     """Create or edit a page."""
     if page_slug is None:
         # First or second routing, create a page.
         ...
     else:
-        # Third or fourth routing, edit a page.
+
         ...
 
-@bp.route('/<page_slug>/delete')
-@bp.route('/<parent_slug>/<page_slug>/delete')
+# @bp.route('/<page_slug>/delete')
+# @bp.route('/<parent_slug>/<page_slug>/delete')
 def page_delete(page_slug, parent_slug=None):
     """Delete a page."""
     ...
