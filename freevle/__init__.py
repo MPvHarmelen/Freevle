8ee30cd9427176c1eca4e595a8b37198864efee9
Martin van Harmelen <Martin@vanharmelen.com>
2013-10-09 10:00:52 +0200

    Fixed adding admin urls from other blueprints

--- freevle/__init__.py
+++ freevle/__init__.py
@@ -1,5 +1,5 @@
 import sys, os
-import importlib
+from importlib import import_module
 
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
@@ -31,12 +31,17 @@ babel = Babel(app)
 csrf = SeaSurf(app)
 
 # Find and import blueprints.
-blueprints = os.listdir(app.config['BLUEPRINTS_DIRECTORY'])
+blueprints = (bp for bp in os.listdir(app.config['BLUEPRINTS_DIRECTORY']) if bp != 'admin')
 for bp_name in blueprints:
     try:
-        bp = importlib.import_module('freevle.blueprints.' + bp_name)
+        bp = import_module('freevle.blueprints.' + bp_name)
         app.register_blueprint(bp.bp, url_prefix=bp.URL_PREFIX)
     except ImportError:
         raise ImportError("{} blueprint appears to be broken.".format(bp_name))
     except AttributeError:
         raise ImportError("{} blueprint doesn't have a blueprint.".format(bp_name))
+
+# Admin needs to be registered as last, so other blueprints can register
+# admin urls.
+admin = import_module('freevle.blueprints.admin')
+app.register_blueprint(admin.bp, url_prefix=admin.URL_PREFIX)
