27dfde48d5ede84fa801eec71fc949e54c77ca0c
Martin van Harmelen <Martin@freevle.org>
2013-10-15 12:05:30 +0200

    Wrote Event class in news models

--- freevle/blueprints/news/models.py
+++ freevle/blueprints/news/models.py
@@ -1,1 +1,10 @@
 from freevle import db
+
+class NewsItem(db.Model):
+	...
+
+class Event(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(EVENT_NAME_LENGTH), nullable=False)
+	date = db.Column(db.Date(), nullable=False)
+	# link = Same as 'relevant' thing in cms.
