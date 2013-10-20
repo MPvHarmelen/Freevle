63f4499ab8ba6432fc166acd9ded89140d59c299
Martin van Harmelen <Martin@vanharmelen.com>
2013-10-20 12:25:09 +0200

    Fixed dynamic admin bug

---
+++ freevle/utils/functions.py
@@ -0,0 +1,9 @@
+"""Functions that come in handy"""
+
+import re
+
+first_cap_re = re.compile('(.)([A-Z][a-z]+)')
+all_cap_re = re.compile('([a-z0-9])([A-Z])')
+def camel_to_underscore(name):
+    s1 = first_cap_re.sub(r'\1_\2', name)
+    return all_cap_re.sub(r'\1_\2', s1).lower()
