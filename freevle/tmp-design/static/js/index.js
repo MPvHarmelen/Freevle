bbca8c76e8fcee1ad5f74d92a96373f38f312c99
Floris Jansen <florismartijnjansen@gmail.com>
2013-10-20 11:45:40 +0200

    make cover-height responsive (fixes #141)

--- freevle/tmp-design/static/js/index.js
+++ freevle/tmp-design/static/js/index.js
@@ -8,6 +8,24 @@ $(document).ready(function() {
     }
   }
 
+  function coverBottom() {
+    var liHeight = 0;
+    $('ul#quicknav > li').each(function() {
+      if($(this).outerHeight() > liHeight) {
+        liHeight = $(this).outerHeight();
+      }
+    });
+    if($('body').innerWidth() > 999) {
+      $('#indexcover').css('margin-bottom', -liHeight-24);
+      $('ul#quicknav > li').css('height', liHeight);
+    } else {
+      $('#indexcover').css('margin-bottom', 0);
+      $('ul#quicknav > li').css('height', 'auto');
+    }
+  }
+
+  coverBottom();
+
   if($(window).width() > 1000) {
     $(window).scroll(function() {
       hideOrShowCygnus($(window).scrollTop(), 180);
@@ -17,5 +35,9 @@ $(document).ready(function() {
       hideOrShowCygnus($(window).scrollTop(), 120);
     });
   }
+
+  $(window).resize(function() {
+    coverBottom();
+  });
 
 });
