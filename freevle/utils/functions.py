845b92431132c8b415e3c651d808dca65b38f0e6
Martin van Harmelen <Martin@vanharmelen.com>
2013-10-21 09:38:18 +0200

    Copied slugify function from the internet
    I'm not using it yet, maybe I will never.

--- freevle/utils/functions.py
+++ freevle/utils/functions.py
@@ -1,9 +1,23 @@
 """Functions that come in handy"""
 
 import re
+from unicodedata import normalize
 
 first_cap_re = re.compile('(.)([A-Z][a-z]+)')
 all_cap_re = re.compile('([a-z0-9])([A-Z])')
 def camel_to_underscore(name):
     s1 = first_cap_re.sub(r'\1_\2', name)
     return all_cap_re.sub(r'\1_\2', s1).lower()
+
+_punct_re = re.compile(r'[\t !"#$%&\'()*\-/<=>?@\[\\\]^_`{|},.]+')
+def slugify(text, delim='-'):
+    """
+    Generates an ASCII-only slug.
+    Snippet from http://flask.pocoo.org/snippets/5/
+    """
+    result = []
+    for word in _punct_re.split(text.lower()):
+        word = normalize('NFKD', word).encode('ascii', 'ignore')
+        if word:
+            result.append(word)
+    return delim.join(result)
