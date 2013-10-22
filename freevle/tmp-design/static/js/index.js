3f1246ed82fbe5ebcf612fcbc5d4c72333eb71f2
Floris Jansen <florismartijnjansen@gmail.com>
2013-10-22 14:05:10 +0200

    improve cygnus gymnasium in header and remove script for yepshow

--- freevle/tmp-design/static/js/index.js
+++ freevle/tmp-design/static/js/index.js
@@ -1,40 +1,22 @@
+function coverBottom() {
+  var liHeight = 0;
+  $('ul#quicknav > li').each(function() {
+    if($(this).outerHeight() > liHeight) {
+      liHeight = $(this).outerHeight();
+    }
+  });
+  if($('body').innerWidth() > 999) {
+    $('#indexcover').css('margin-bottom', -liHeight-24);
+    $('ul#quicknav > li').css('min-height', liHeight);
+  } else {
+    $('#indexcover').css('margin-bottom', 0);
+    $('ul#quicknav > li').css('min-height', 0);
+  }
+}
+
 $(document).ready(function() {
 
-  function hideOrShowCygnus(toTop,when) {
-    if(toTop > when) {
-      $('header h1').addClass('yepshow');
-    } else {
-      $('header h1').removeClass('yepshow');
-    }
-  }
-
-  function coverBottom() {
-    var liHeight = 0;
-    $('ul#quicknav > li').each(function() {
-      if($(this).outerHeight() > liHeight) {
-        liHeight = $(this).outerHeight();
-      }
-    });
-    if($('body').innerWidth() > 999) {
-      $('#indexcover').css('margin-bottom', -liHeight-24);
-      $('ul#quicknav > li').css('min-height', liHeight);
-    } else {
-      $('#indexcover').css('margin-bottom', 0);
-      $('ul#quicknav > li').css('min-height', 0);
-    }
-  }
-
   coverBottom();
-
-  if($(window).width() > 1000) {
-    $(window).scroll(function() {
-      hideOrShowCygnus($(window).scrollTop(), 180);
-    });
-  } else {
-    $(window).scroll(function() {
-      hideOrShowCygnus($(window).scrollTop(), 120);
-    });
-  }
 
   $(window).resize(function() {
     coverBottom();
