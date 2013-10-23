9d80ac0982852bb84a97cfc1de069720438de481
Floris Jansen <florismartijnjansen@gmail.com>
2013-10-23 14:08:11 +0200

    fix height .more a in FF

--- freevle/tmp-design/static/js/index.js
+++ freevle/tmp-design/static/js/index.js
@@ -8,9 +8,11 @@ function coverBottom() {
   });
   if($('body').innerWidth() > 999) {
     $('#indexcover').css('margin-bottom', -1 * (liHeight + 24));
+    $('ul#quicknav li.more a').css('height', liHeight);
     $('ul#quicknav > li').css('min-height', liHeight);
   } else {
     $('#indexcover').css('margin-bottom', 0);
+    $('ul#quicknav li.more a').css('height', '100%');
     $('ul#quicknav > li').css('min-height', 0);
   }
 }
