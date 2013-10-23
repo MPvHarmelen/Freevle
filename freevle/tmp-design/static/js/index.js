d363926a9654ae67870de2fd6ec2722017511cb9
Floris Jansen <florismartijnjansen@gmail.com>
2013-10-23 13:54:00 +0200

    try somethings with js, fix #184 pl0x

--- freevle/tmp-design/static/js/index.js
+++ freevle/tmp-design/static/js/index.js
@@ -1,12 +1,17 @@
-function coverBottom() {
+function coverBottom(first) {
   var liHeight = 0;
   $('ul#quicknav > li').each(function() {
     if($(this).outerHeight() > liHeight) {
-      liHeight = $(this).outerHeight();
+      liHeight = Math.round($(this).outerHeight());
     }
+    console.log(liHeight);
   });
   if($('body').innerWidth() > 999) {
-    $('#indexcover').css('margin-bottom', -liHeight-24);
+    if(first) {
+      liHeight = liHeight - (1.8 * 19);
+    }
+
+    $('#indexcover').css('margin-bottom', -1 * (liHeight + 24));
     $('ul#quicknav > li').css('min-height', liHeight);
   } else {
     $('#indexcover').css('margin-bottom', 0);
@@ -16,7 +21,7 @@ function coverBottom() {
 
 $(document).ready(function() {
 
-  coverBottom();
+  coverBottom(true);
 
   $(window).resize(function() {
     coverBottom();
