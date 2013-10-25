d37f9b01b91cc7fb00b922779fed678b2bb78ca4
Martin van Harmelen <Martin@vanharmelen.com>
2013-10-25 08:32:29 +0200

    Added news to add_debug_data and
    improved speed for cms part

--- add_debug_data.py
+++ add_debug_data.py
@@ -1,8 +1,9 @@
 from datetime import date, timedelta
 
 from freevle import db, app
-from freevle.blueprints.cms.tests import CMSTests
-from freevle.blueprints.cms.models import Event
+# from freevle.blueprints.cms.tests import CMSTests
+from freevle.blueprints.cms.models import Category, Subcategory, Page, TextSection, ImageSection, Event
+from freevle.blueprints.news.models import NewsItem
 # from freevle.blueprints.news.models import NewsItem, Event
 # from freevle.blueprints.galleries.models import Album
 
@@ -12,6 +13,7 @@ NUMBER_OF_PAGES_PER_SUBCATEGORIE = 4
 NUMBER_OF_UNPROTECTED_CATEGORIES = 1
 NUMBER_OF_TEACHER_PROTECTED_CATEGORIES = 2
 NUMBER_OF_EVENTS = 100
+NUMBER_OF_NEWS_ITEMS = 100
 
 image_urls = ['/static/img/sections/' + name for name in ['page.jpg', 'newsfront.png', 'page2.jpg']]
 html_classes = ['school', 'education', 'activities', 'intern', 'more', 'promotion']
@@ -28,70 +30,125 @@ lorum = """Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac viv
 Vestibulum sodales molestie ligula a consectetur. Praesent eleifend, sem non auctor ultricies, massa sem dictum nibh, vitae tempor elit dui eu augue. Suspendisse imperdiet risus et libero molestie pellentesque. Aliquam gravida nibh nunc, at ultricies elit tincidunt at. Morbi non sem tempor, semper leo a, pulvinar lectus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed accumsan a sapien eget hendrerit. Duis urna erat, tincidunt at tincidunt in, faucibus et felis. In hac habitasse platea dictumst. Curabitur eleifend ante sit amet neque luctus, sit amet dictum nulla vestibulum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."""
 
 
-cms_tests = CMSTests()
-def create_page_data():
-    for cat_num in range(NUMBER_OF_CATEGORIES):
+# cms_tests = CMSTests()
+def create_page_data(cat_amount, subcat_amount, page_amount):
+    for cat_num in range(cat_amount):
         cat_name = 'test-{}'.format(cat_num)
         if cat_num < NUMBER_OF_UNPROTECTED_CATEGORIES:
             security_level = None
-        elif NUMBER_OF_CATEGORIES - cat_num <= NUMBER_OF_TEACHER_PROTECTED_CATEGORIES:
+        elif cat_amount - cat_num <= NUMBER_OF_TEACHER_PROTECTED_CATEGORIES:
             security_level = 'teacher'
         else:
             security_level = 'student'
-        cat = cms_tests.create_category(
-            cat_name,
-            cat_name,
-            html_classes[cat_num % len(html_classes)],
-            security_level
+        cat = Category(
+            title=cat_name,
+            slug=cat_name,
+            html_class=html_classes[cat_num % len(html_classes)],
+            security_level=security_level
         )
-        print('Made category:\t', cat)
-        for subcat_num in range(NUMBER_OF_SUBCATEGORIES_PER_CATEGORIE):
+        db.session.add(cat)
+        print('Added category:\t', cat)
+        for subcat_num in range(subcat_amount):
             subcat_name = 'test-{}'.format(subcat_num)
-            subcat = cms_tests.create_subcategory(
-                subcat_name,
-                subcat_name,
+            subcat = Subcategory(
+                title=subcat_name,
+                slug=subcat_name,
                 category=cat,
                 html_class=html_classes[(cat_num + subcat_num) % len(html_classes)] if not security_level else None
             )
-            print('Made subcategory:\t', subcat)
-            for page_num in range(NUMBER_OF_PAGES_PER_SUBCATEGORIE):
-                p = cms_tests.create_page(
-                    'TeSt{}'.format(page_num),
-                    'test{}-{}'.format(subcat_num, page_num),
-                    subcat,
+            db.session.add(subcat)
+            print('Added subcategory:\t', subcat)
+            for page_num in range(page_amount):
+                p = Page(
+                    subcategory=subcat,
+                    title='TeSt{}'.format(page_num),
+                    slug='test{}-{}'.format(subcat_num, page_num),
                     content=page_content.format(page_num),
-                    cover_image_url=image_urls[(page_num + subcat_num + cat_num) % len(image_urls)]
+                    cover_image_url=image_urls[(page_num + subcat_num + cat_num) % len(image_urls)],
+                    is_published=True
                 )
-                print('Made page:\t', p)
+                db.session.add(p)
+                print('Added page:\t', p)
                 for y in range(2):
-                    s = cms_tests.create_text_section(order=y, slug='section-{}'.format(y),
-                                                  page=p, content=lorum)
-                    print('Made section:\t', s)
-                s = cms_tests.create_image_section(order=2, slug='image1', page=p,
-                                               image_url=image_urls[1])
-                print('Made section:\t', s)
-                s = cms_tests.create_text_section(order=3, slug='section-2'.format(y),
-                                                  page=p, content=lorum)
-                print('Made section:\t', s)
-                s = cms_tests.create_image_section(order=4, slug='image2', page=p,
-                                               image_url=image_urls[2])
-                print('Made section:\t', s)
-                s = cms_tests.create_text_section(order=5, slug='section-3'.format(y),
-                                                  page=p, content=lorum)
-                print('Made section:\t', s)
+                    s = TextSection(
+                        order=y,
+                        title='section-{}'.format(y),
+                        slug='section-{}'.format(y),
+                        page=p,
+                        content=lorum)
+                    db.session.add(s)
+                    print('Added section:\t', s)
+                s = ImageSection(
+                    order=2,
+                    title='image1',
+                    slug='image1',
+                    page=p,
+                    image_url=image_urls[1]
+                )
+                db.session.add(s)
+                print('Added section:\t', s)
+                s = TextSection(
+                    order=3,
+                    title='section-2',
+                    slug='section-2',
+                    page=p,
+                    content=lorum
+                )
+                db.session.add(s)
+                print('Added section:\t', s)
+                s = ImageSection(
+                    order=4,
+                    title='image2',
+                    slug='image2',
+                    page=p,
+                    image_url=image_urls[2]
+                )
+                db.session.add(s)
+                print('Added section:\t', s)
+                s = TextSection(
+                    order=5,
+                    title='section-3',
+                    slug='section-3',
+                    page=p,
+                    content=lorum
+                )
+                db.session.add(s)
+                print('Added section:\t', s)
+    print('Commit!')
+    db.session.commit()
 
 def create_events(number):
+    today = date.today()
     for num in range(number):
-        event = Event(title='Nice event', date=date.today() + timedelta(days=num))
+        event = Event(title='Nice event', date=today + timedelta(days=num))
         db.session.add(event)
         print('Added {} to session.'.format(event))
+    print('Commit!')
+    db.session.commit()
+
+def create_news(amount):
+    today = date.today()
+    for num in range(amount):
+        item = NewsItem(
+            title='NewsItem {}'.format(num),
+            slug='NewsItem-{}'.format(num),
+            content=lorum,
+            cover_image_url=(image_urls[num % len(image_urls)], None)[num % 2],
+            date_published=today + timedelta(days=num)
+        )
+        db.session.add(item)
+        print('Added {} to session.'.format(item))
     print('Commit!')
     db.session.commit()
 
 if __name__ == '__main__':
     if input('Create page data? (y/N) ').lower() in ['y', 'yes', 'yep', 'yeah']:
         with app.app_context():
-            create_page_data()
+            create_page_data(NUMBER_OF_CATEGORIES,
+                             NUMBER_OF_SUBCATEGORIES_PER_CATEGORIE,
+                             NUMBER_OF_PAGES_PER_SUBCATEGORIE)
     if input('Create events? (y/N) ').lower() in ['y', 'yes', 'yep', 'yeah']:
         create_events(NUMBER_OF_EVENTS)
+    if input('Create news? (y/N) ').lower() in ['y', 'yes', 'yep', 'yeah']:
+        create_news(NUMBER_OF_NEWS_ITEMS)
     print('Done.')
