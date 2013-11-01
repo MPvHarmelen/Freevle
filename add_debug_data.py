571521fe80a19dbb3b5153048c328a49ff315114
Martin van Harmelen <Martin@vanharmelen.com>
2013-11-01 17:38:41 +0100

    Improved add_debug_data.py and added albums

--- add_debug_data.py
+++ add_debug_data.py
@@ -4,18 +4,28 @@ from freevle import db, app
 # from freevle.blueprints.cms.tests import CMSTests
 from freevle.blueprints.cms.models import Category, Subcategory, Page, TextSection, ImageSection, Event
 from freevle.blueprints.news.models import NewsItem
-# from freevle.blueprints.news.models import NewsItem, Event
-# from freevle.blueprints.galleries.models import Album
+from freevle.blueprints.galleries.models import Album, Image
 
 NUMBER_OF_CATEGORIES = 7
-NUMBER_OF_SUBCATEGORIES_PER_CATEGORIE = 3
-NUMBER_OF_PAGES_PER_SUBCATEGORIE = 4
+SUBCATEGORIES_PER_CATEGORIE = 3
+PAGES_PER_SUBCATEGORIE = 4
 NUMBER_OF_UNPROTECTED_CATEGORIES = 1
 NUMBER_OF_TEACHER_PROTECTED_CATEGORIES = 2
 NUMBER_OF_EVENTS = 100
 NUMBER_OF_NEWS_ITEMS = 100
+NUMBER_OF_ALBUMS = 100
+IMAGES_PER_ALBUM = 100
+TODAY = date.today()
 
-image_urls = ['/static/img/sections/' + name for name in ['page.jpg', 'newsfront.png', 'page2.jpg']]
+def add(item):
+    db.session.add(item)
+    print('Added {} to session.'.format(item))
+
+def commit():
+    print('Commit!')
+    db.session.commit()
+
+image_urls = ['/static/img/sections/' + name for name in ['page.jpg', 'newsfront.png', 'page2.jpg', 'paralax3.jpg']]
 html_classes = ['school', 'education', 'activities', 'intern', 'more', 'promotion']
 authors = ['Floris Jansen', 'Pim ten Thije', 'Martin van Harmelen', 'Jan Modaal']
 
@@ -38,8 +48,6 @@ lorum_with_image = """Lorem _ipsum dolor_ sit amet, *consectetur* adipiscing eli
 # Vestibulum
 sodales molestie ligula a consectetur. Praesent eleifend, sem non auctor ultricies, massa sem dictum nibh, vitae tempor elit dui eu augue. Suspendisse imperdiet risus et libero molestie pellentesque. Aliquam gravida nibh nunc, at ultricies elit tincidunt at. Morbi non sem tempor, semper leo a, pulvinar lectus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed accumsan a sapien eget hendrerit. Duis urna erat, tincidunt at tincidunt in, faucibus et felis. In hac habitasse platea dictumst. Curabitur eleifend ante sit amet neque luctus, sit amet dictum nulla vestibulum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."""
 
-
-# cms_tests = CMSTests()
 def create_page_data(cat_amount, subcat_amount, page_amount):
     for cat_num in range(cat_amount):
         cat_name = 'test-{}'.format(cat_num)
@@ -55,8 +63,7 @@ def create_page_data(cat_amount, subcat_amount, page_amount):
             html_class=html_classes[cat_num % len(html_classes)],
             security_level=security_level
         )
-        db.session.add(cat)
-        print('Added category:\t', cat)
+        add(cat)
         for subcat_num in range(subcat_amount):
             subcat_name = 'test-{}'.format(subcat_num)
             subcat = Subcategory(
@@ -65,8 +72,7 @@ def create_page_data(cat_amount, subcat_amount, page_amount):
                 category=cat,
                 html_class=html_classes[(cat_num + subcat_num) % len(html_classes)] if not security_level else None
             )
-            db.session.add(subcat)
-            print('Added subcategory:\t', subcat)
+            add(subcat)
             for page_num in range(page_amount):
                 p = Page(
                     subcategory=subcat,
@@ -76,27 +82,22 @@ def create_page_data(cat_amount, subcat_amount, page_amount):
                     cover_image_url=image_urls[(page_num + subcat_num + cat_num) % len(image_urls)],
                     is_published=True
                 )
-                db.session.add(p)
-                print('Added page:\t', p)
+                add(p)
                 for y in range(2):
-                    s = TextSection(
+                    add(TextSection(
                         order=y,
                         title='section-{}'.format(y),
                         slug='section-{}'.format(y),
                         page=p,
-                        content=lorum)
-                    db.session.add(s)
-                    print('Added section:\t', s)
-                s = ImageSection(
+                        content=lorum))
+                add(ImageSection(
                     order=2,
                     title='image1',
                     slug='image1',
                     page=p,
                     image_url=image_urls[1]
-                )
-                db.session.add(s)
-                print('Added section:\t', s)
-                s = TextSection(
+                ))
+                add(TextSection(
                     order=3,
                     title='section-2',
                     slug='section-2',
@@ -104,63 +105,81 @@ def create_page_data(cat_amount, subcat_amount, page_amount):
                     content=lorum_with_image.format(image_urls[
                         (page_num + subcat_num + cat_num) % len(image_urls)
                     ])
-                )
-                db.session.add(s)
-                print('Added section:\t', s)
-                s = ImageSection(
+                ))
+                add(ImageSection(
                     order=4,
                     title='image2',
                     slug='image2',
                     page=p,
                     image_url=image_urls[2]
-                )
-                db.session.add(s)
-                print('Added section:\t', s)
-                s = TextSection(
+                ))
+                add(TextSection(
                     order=5,
                     title='section-3',
                     slug='section-3',
                     page=p,
                     content=lorum
-                )
-                db.session.add(s)
-                print('Added section:\t', s)
-    print('Commit!')
-    db.session.commit()
+                ))
+    commit()
 
 def create_events(number):
-    today = date.today()
     for num in range(number):
-        event = Event(title='Nice event', date=today + timedelta(days=num))
-        db.session.add(event)
-        print('Added {} to session.'.format(event))
-    print('Commit!')
-    db.session.commit()
+        add(Event(title='Nice event', date=TODAY + timedelta(days=num)))
+    commit()
 
 def create_news(amount):
-    today = date.today()
     for num in range(amount):
-        item = NewsItem(
+        add(NewsItem(
             title='NewsItem {}'.format(num),
             slug='NewsItem-{}'.format(num),
             author=authors[num % len(authors)],
             content=lorum_with_image.format(image_urls[(num + 1) % len(image_urls)]),
             cover_image_url=(image_urls[num % len(image_urls)], None)[num % 2],
-            date_published=today - timedelta(days=num)
+            date_published=TODAY - timedelta(days=num)
+        ))
+    commit()
+
+def create_albums(album_amount, image_amount):
+    for album_num in range(album_amount):
+        name = 'album-{}'.format(album_num)
+        album = Album(
+            title=name,
+            slug=name,
+            date_published=TODAY - timedelta(days=100 * album_num)
         )
-        db.session.add(item)
-        print('Added {} to session.'.format(item))
-    print('Commit!')
-    db.session.commit()
+        add(album)
+        for image_num in range(image_amount):
+            name = 'image-{}'.format(image_num)
+            add(Image(
+                title=name,
+                slug=name,
+                album=album,
+                order=image_num,
+                image_url=image_urls[(image_num + album_num) % len(image_urls)]
+            ))
+    commit()
 
 if __name__ == '__main__':
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser()
+    parser.add_argument('configuration', help='Relative path to configuration file', nargs='?')
+    parser.add_argument('-v', '--verbose', action='store_true', help='Verbose option.')
+    parsed_args = parser.parse_args()
+
+    print = print if parsed_args.verbose else lambda *args, **kwargs: None
+    # def print(*args, **kwargs):
+    #     if parsed_args.verbose: old_print(*args, **kwargs)
+
     if input('Create page data? (y/N) ').lower() in ['y', 'yes', 'yep', 'yeah']:
         with app.app_context():
             create_page_data(NUMBER_OF_CATEGORIES,
-                             NUMBER_OF_SUBCATEGORIES_PER_CATEGORIE,
-                             NUMBER_OF_PAGES_PER_SUBCATEGORIE)
+                             SUBCATEGORIES_PER_CATEGORIE,
+                             PAGES_PER_SUBCATEGORIE)
     if input('Create events? (y/N) ').lower() in ['y', 'yes', 'yep', 'yeah']:
         create_events(NUMBER_OF_EVENTS)
     if input('Create news? (y/N) ').lower() in ['y', 'yes', 'yep', 'yeah']:
         create_news(NUMBER_OF_NEWS_ITEMS)
+    if input('Create albums? (y/N) ').lower() in ['y', 'yes', 'yep', 'yeah']:
+        create_albums(NUMBER_OF_ALBUMS, IMAGES_PER_ALBUM)
     print('Done.')
