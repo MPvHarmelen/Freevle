6f12b05bd79b77d1b136b269d2013f5f541e9a0b
Martin van Harmelen <Martin@vanharmelen.com>
2013-11-16 15:05:08 +0100

    Fixed news part of #239

--- freevle/blueprints/news/views.py
+++ freevle/blueprints/news/views.py
@@ -1,13 +1,27 @@
 from datetime import date, datetime
 from flask import render_template, request, Markup
-from sqlalchemy import func, extract
+from sqlalchemy import extract
 
 from freevle import db
 from freevle.utils.functions import headles_markdown as markdown
-from freevle.utils.decorators import paginated_view, archived_view
+from freevle.utils.functions import paginate as _paginate
+from freevle.utils.decorators import archived_view
+
 from . import bp
 from .constants import NEWS_ITEMS_PER_PAGE, NEWS_PREVIEW_LENGTH
 from .models import NewsItem
+
+def process_news(news):
+    for news_item in news:
+        # Content preview will be at most NEWS_PREVIEW_LENGTH chars and will end
+        # at the end of the last sentence.
+        news_item.content = news_item.content[:NEWS_PREVIEW_LENGTH].rsplit('.', 1)[0] + '.'
+        # The closing </p> is added in the template
+        news_item.content = Markup(markdown(news_item.content)[:-4])
+    return news
+
+paginate = lambda all_items: _paginate(all_items, NEWS_ITEMS_PER_PAGE,
+                                       process_news)
 
 @bp.context_processor
 def inject_breadcrumbs():
@@ -25,23 +39,12 @@ def inject_breadcrumbs():
         breadcrumbs.append((url_sections[-1], ''))
     return dict(breadcrumbs=breadcrumbs)
 
-def process_news(news):
-    for news_item in news:
-        # Content preview will be at most NEWS_PREVIEW_LENGTH chars and will end
-        # at the end of the last sentence.
-        news_item.content = news_item.content[:NEWS_PREVIEW_LENGTH].rsplit('.', 1)[0] + '.'
-        # The closing </p> is added in the template
-        news_item.content = Markup(markdown(news_item.content)[:-4])
-    return news
-
-@paginated_view(NEWS_ITEMS_PER_PAGE, extra_function=process_news)
-def query_news_page(news_query):
-    return news_query.order_by(NewsItem.date_published.desc())
-
 @bp.route('/')
 def overview():
-    query = NewsItem.query.filter(NewsItem.date_published <= date.today())
-    news, page, max_page = query_news_page(query)
+    news = NewsItem.query.filter(NewsItem.date_published <= date.today()).\
+           order_by(NewsItem.date_published.desc()).\
+           all()
+    news, page, max_page = paginate(news)
     return render_template('news/news.html', news=news, page=page,
                            max_page=max_page)
 
@@ -55,7 +58,8 @@ def archive(year=None, month=None):
         query = query.filter(extract('year', NewsItem.date_published) == year)
         if month is not None:
             query = query.filter(extract('month', NewsItem.date_published) == month)
-    news, page, max_page = query_news_page(query)
+    news = query.order_by(NewsItem.date_published.desc()).all()
+    news, page, max_page = paginate(news)
 
     oldest_date = db.session.query(NewsItem.date_published).\
                  order_by(NewsItem.date_published.asc()).first()[0]
@@ -68,6 +72,8 @@ def archive(year=None, month=None):
         page=page,
         max_page=max_page
     )
+
+
 @bp.route('/<int:year>/<int:month>/<int:day>/<slug>')
 def item_view(year, month, day, slug):
     requested_date = datetime.strptime(
--- freevle/utils/functions.py
+++ freevle/utils/functions.py
@@ -1,8 +1,10 @@
 """Functions that come in handy"""
 
 import re
+from math import ceil
 from unicodedata import normalize
 from markdown import Markdown
+from flask import request
 
 markdown = Markdown(output_format='html5', safe_mode='escape', lazy_ol=False).convert
 
@@ -34,3 +36,43 @@ def slugify(text, delim='-'):
         if word:
             result.append(word)
     return delim.join(result)
+
+def paginate(all_items, items_per_page=10, extra_function=lambda a: a):
+    try:
+        page = int(request.args.get('page', 1))
+    except ValueError as e:
+        if not app.config['DEBUG']:
+            raise NotFound
+        else:
+            raise NotFound(e)
+    # if page < 0:
+    #     raise NotFound
+    if page > 0:
+        page -= 1 # Make 'page' zero based
+
+    # list slicing lesson:
+    # >>> li = [0,1,2,3,4,5,6,7,8,9]
+    # >>> li[-2]
+    # 8
+    # >>> li[-2:0]
+    # []
+    # >>> li[-2:]
+    # [8, 9]
+    # >>> li[-2:None]
+    # [8, 9]
+    # Those last two results are what we're looking for.
+    # That's why the if statement is in there.
+    max_page = int(ceil(len(all_items) / items_per_page))
+    items = all_items[
+        items_per_page * page :
+        items_per_page * (page + 1) if page != -1 else None
+    ]
+    if page < 0:
+        # Make negative lookup positive again
+        page += max_page
+    if (max_page != 0 and abs(page) > max_page)\
+       or abs(page) > max_page + 1:
+        raise NotFound
+    # Make 'page' one based again
+    page += 1
+    return extra_function(items), page, max_page
